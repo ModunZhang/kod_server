c45075bf7daa58baafb2c23cdf76d9a3cbb53736
Modun Zhang <modunzhang@gmail.com>
2014-08-03 22:11:35 +0800

    Config sever port

--- game-server/app.js
+++ game-server/app.js
@@ -2,9 +2,16 @@ var pomelo = require("pomelo")
 var redis = require("redis")
 var mongoose = require("mongoose")
 var globalChannel = require('pomelo-globalchannel-plugin')
+var GameDefined = require("./config/gameDefined")
 
 var app = pomelo.createApp()
 app.set('name', 'KODServer')
+
+var env = app.get("env")
+var redisHost = GameDefined[env].redis.host
+var redisPort = GameDefined[env].redis.port
+var mongoHost = GameDefined[env].mongo.host
+var mongoPort = GameDefined[env].mongo.port
 
 app.configure('production|development', 'gate', function(){
 	app.set('connectorConfig', {
@@ -24,21 +31,21 @@ app.configure('production|development', 'logic', function(){
 	})
 
 	app.use(globalChannel, {globalChannel:{
-		host:'127.0.0.1',
-		port:6379,
+		host:redisHost,
+		port:redisPort,
 		db:'1'
 	}})
 
-	var redisClient = redis.createClient(6379)
+	var redisClient = redis.createClient(redisPort, redisHost)
 	app.set('redis', redisClient)
-	var mongooseClient = mongoose.connect('mongodb://127.0.0.1:27017/kod')
+	var mongooseClient = mongoose.connect('mongodb://'+ mongoHost + ":" + mongoPort +'/kod')
 	app.set('mongoose', mongooseClient)
 })
 
 app.configure('production|development', 'chat', function(){
-	var redisClient = redis.createClient(6379)
+	var redisClient = redis.createClient(redisPort, redisHost)
 	app.set('redis', redisClient)
-	var mongooseClient = mongoose.connect('mongodb://127.0.0.1:27017/kod')
+	var mongooseClient = mongoose.connect('mongodb://'+ mongoHost + ":" + mongoPort +'/kod')
 	app.set('mongoose', mongooseClient)
 })
 
