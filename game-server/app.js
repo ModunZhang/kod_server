648ecf00732e5237e8f3e08a85586b1f37670a6a
Modun Zhang <modunzhang@gmail.com>
2014-08-04 15:43:10 +0800

    config for aws

--- game-server/app.js
+++ game-server/app.js
@@ -6,9 +6,6 @@ var globalChannel = require("pomelo-globalchannel-plugin")
 var app = pomelo.createApp()
 app.set("name", "KODServer")
 
-//app.configure("production|development", function(){
-//	app.set("ssh_config_params", "-i /home/ec2-user/.ssh/AWS.pem")
-//})
 
 app.configure("production|development", "gate", function(){
 	app.set("connectorConfig", {
@@ -58,20 +55,20 @@ app.configure("production", "logic", function(){
 	var mongooseClient = mongoose.connect("mongodb://127.0.0.1:27017/kod")
 	app.set("mongoose", mongooseClient)
 })
-//
-//app.configure("development", "chat", function(){
-//	var redisClient = redis.createClient(6379, "127.0.0.1")
-//	app.set("redis", redisClient)
-//	var mongooseClient = mongoose.connect("mongodb://127.0.0.1:27017/kod")
-//	app.set("mongoose", mongooseClient)
-//})
-//
-//app.configure("production", "chat", function(){
-//	var redisClient = redis.createClient(6379, "172.31.15.88")
-//	app.set("redis", redisClient)
-//	var mongooseClient = mongoose.connect("mongodb://127.0.0.1:27017/kod")
-//	app.set("mongoose", mongooseClient)
-//})
+
+app.configure("development", "chat", function(){
+	var redisClient = redis.createClient(6379, "127.0.0.1")
+	app.set("redis", redisClient)
+	var mongooseClient = mongoose.connect("mongodb://127.0.0.1:27017/kod")
+	app.set("mongoose", mongooseClient)
+})
+
+app.configure("production", "chat", function(){
+	var redisClient = redis.createClient(6379, "172.31.15.88")
+	app.set("redis", redisClient)
+	var mongooseClient = mongoose.connect("mongodb://127.0.0.1:27017/kod")
+	app.set("mongoose", mongooseClient)
+})
 
 app.start()
 
