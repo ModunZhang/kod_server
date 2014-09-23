7874173daff7656c6bde0d4e50726f0d62846248
Modun Zhang <modunzhang@gmail.com>
2014-09-23 15:56:04 +0800

    dev of alliance & bugfix for dragon schema

---
+++ game-server/app/consts/define.js
@@ -0,0 +1,12 @@
+"use strict"
+
+/**
+ * Created by modun on 14-9-23.
+ */
+
+module.exports = {
+	RequestJoinAllianceMessageMaxSize:5,//申请加入联盟消息申请最大数量
+	InviteJoinAllianceMessageMaxSize:5,//邀请加入联盟消息最大数量
+	JoinAllianceMessageExpireTime:12*60*60*1000,//邀请或申请的过期时间
+	AllianceRequestMessageMaxSize:50,//联盟中的入盟申请信息最大数量
+}
