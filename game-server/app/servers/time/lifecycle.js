e019e18bbadb5bd8553407943af690c57140b2cb
Modun Zhang <modunzhang@gmail.com>
2015-01-05 16:05:18 +0800

    dev of restoreing game data

--- game-server/app/servers/time/lifecycle.js
+++ game-server/app/servers/time/lifecycle.js
@@ -43,7 +43,9 @@ life.afterStartup = function(app, callback){
 }
 
 life.beforeShutdown = function(app, callback){
-	callback()
+	DbUtils.unloadAllDataAsync(app.get("playerDao"), app.get("allianceDao")).then(function(){
+		callback()
+	})
 }
 
 life.afterStartAll = function(app){
@@ -62,135 +64,53 @@ life.afterStartAll = function(app){
 			allianceDocs = docs
 		}).then(function(){
 			_.each(playerDocs, function(playerDoc){
-				var key = Consts.TimeEventType.Player + ":" + playerDoc._id
-				var willRemovedBuildingEvents = []
 				_.each(playerDoc.buildingEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedBuildingEvents.push(event)
-						var building = LogicUtils.getBuildingByEvent(playerDoc, event)
-						building.level += 1
-						LogicUtils.updateBuildingsLevel(playerDoc)
-						if(_.isObject(playerDoc.alliance) && !_.isEmpty(playerDoc.alliance.id)){
-							var allianceDoc = GetAllianceDoc(allianceDocs, playerDoc.alliance.id)
-							var helpEvent = LogicUtils.getAllianceHelpEvent(allianceDoc, event.id)
-							if(_.isObject(helpEvent)){
-								LogicUtils.removeItemInArray(allianceDoc.helpEvents, helpEvent)
-							}
-						}
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "buildingEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "buildingEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.buildingEvents, willRemovedBuildingEvents)
-				var willRemovedHouseEvents = []
 				_.each(playerDoc.houseEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedHouseEvents.push(event)
-						var house = LogicUtils.getHouseByEvent(playerDoc, event)
-						house.level += 1
-						if(_.isEqual("dwelling", house.type)){
-							var previous = DataUtils.getDwellingPopulationByLevel(house.level - 1)
-							var next = DataUtils.getDwellingPopulationByLevel(house.level)
-							playerDoc.resources.citizen += next - previous
-							LogicUtils.refreshPlayerResources(playerDoc)
-						}
-						if(_.isObject(playerDoc.alliance) && !_.isEmpty(playerDoc.alliance.id)){
-							var allianceDoc = GetAllianceDoc(allianceDocs, playerDoc.alliance.id)
-							var helpEvent = LogicUtils.getAllianceHelpEvent(allianceDoc, event.id)
-							if(_.isObject(helpEvent)){
-								LogicUtils.removeItemInArray(allianceDoc.helpEvents, helpEvent)
-							}
-						}
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "houseEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "houseEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.houseEvents, willRemovedHouseEvents)
-				var willRemovedTowerEvents = []
 				_.each(playerDoc.towerEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedTowerEvents.push(event)
-						var tower = LogicUtils.getTowerByEvent(playerDoc, event)
-						tower.level += 1
-						if(_.isObject(playerDoc.alliance) && !_.isEmpty(playerDoc.alliance.id)){
-							var allianceDoc = GetAllianceDoc(allianceDocs, playerDoc.alliance.id)
-							var helpEvent = LogicUtils.getAllianceHelpEvent(allianceDoc, event.id)
-							if(_.isObject(helpEvent)){
-								LogicUtils.removeItemInArray(allianceDoc.helpEvents, helpEvent)
-							}
-						}
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "towerEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "towerEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.towerEvents, willRemovedTowerEvents)
-				var willRemovedWallEvents = []
 				_.each(playerDoc.wallEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedWallEvents.push(event)
-						var wall = playerDoc.wall
-						wall.level += 1
-						if(_.isObject(playerDoc.alliance) && !_.isEmpty(playerDoc.alliance.id)){
-							var allianceDoc = GetAllianceDoc(allianceDocs, playerDoc.alliance.id)
-							var helpEvent = LogicUtils.getAllianceHelpEvent(allianceDoc, event.id)
-							if(_.isObject(helpEvent)){
-								LogicUtils.removeItemInArray(allianceDoc.helpEvents, helpEvent)
-							}
-						}
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "wallEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "wallEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.wallEvents, willRemovedWallEvents)
 				_.each(playerDoc.materialEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						event.finishTime = 0
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "materialEvents", event.id, event.finishTime - now))
+					if(event.finishTime > 0){
+						event.finishTime = now + (event.finishTime - event.startTime)
+						event.startTime = now
+						eventFuncs.push(addTimeEventAsync(eventServerId, key, "materialEvents", event.id, event.finishTime - event.startTime))
 					}
 				})
-				var willRemovedSoldierEvents = []
 				_.each(playerDoc.soldierEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedSoldierEvents.push(event)
-						playerDoc.soldiers[event.name] += event.count
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "soldierEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "soldierEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.soldierEvents, willRemovedSoldierEvents)
-				var willRemovedDragonEquipmentEvents = []
 				_.each(playerDoc.dragonEquipmentEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedDragonEquipmentEvents.push(event)
-						playerDoc.dragonEquipments[event.name] += 1
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "dragonEquipmentEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "dragonEquipmentEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.dragonEquipmentEvents, willRemovedDragonEquipmentEvents)
-				var willRemovedTreatSoldierEvents = []
 				_.each(playerDoc.treatSoldierEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedTreatSoldierEvents.push(event)
-						_.each(event.soldiers, function(soldier){
-							playerDoc.soldiers[soldier.name] += soldier.count
-						})
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "treatSoldierEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "treatSoldierEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.treatSoldierEvents, willRemovedTreatSoldierEvents)
-				var willRemovedCoinEvents = []
 				_.each(playerDoc.coinEvents, function(event){
-					if(event.finishTime > 0 && event.finishTime <= now){
-						willRemovedCoinEvents.push(event)
-						playerDoc.resources.coin += event.coin
-					}else if(event.finishTime > 0){
-						eventFuncs.push(addTimeEventAsync(eventServerId, key, "coinEvents", event.id, event.finishTime - now))
-					}
+					event.finishTime = now + (event.finishTime - event.startTime)
+					event.startTime = now
+					eventFuncs.push(addTimeEventAsync(eventServerId, key, "coinEvents", event.id, event.finishTime - event.startTime))
 				})
-				LogicUtils.removeItemsInArray(playerDoc.coinEvents, willRemovedCoinEvents)
 				LogicUtils.refreshPlayerPower(playerDoc)
 			})
 		}).then(function(){
@@ -227,7 +147,7 @@ life.afterStartAll = function(app){
 }
 
 var GetAllianceDoc = function(allianceDocs, allianceId){
-	for(var i = 0; i < allianceDocs.length; i ++){
+	for(var i = 0; i < allianceDocs.length; i++){
 		var allianceDoc = allianceDocs[i]
 		if(_.isEqual(allianceDoc._id, allianceId)) return allianceDoc
 	}
