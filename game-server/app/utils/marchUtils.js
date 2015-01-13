c65f458847e12ed6680f5e656ab82cbd2ce59de9
Modun Zhang <modunzhang@gmail.com>
2015-01-13 10:08:48 +0800

    debug

--- game-server/app/utils/marchUtils.js
+++ game-server/app/utils/marchUtils.js
@@ -43,6 +43,7 @@ var getDistance = function(width, height){
 var getMarchTime = function(playerDoc, width, height){
 	var distance = getDistance(width, height)
 	console.log(width, height, "---------------------------------------------")
+	console.log(distance, "-----------------------------------------")
 	var time = AllianceInit.intInit.allianceRegionMapBaseTimePerGrid.value * distance * 1000
 	return time//5 * 1000
 }
