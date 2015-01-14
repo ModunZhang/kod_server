52f8049b39198c812049b4e6478604df82b1aa52
Modun Zhang <modunzhang@gmail.com>
2015-01-14 20:00:32 +0800

    modify configs

---
+++ game-server/app/datas/Items_items.js
@@ -0,0 +1,145 @@
+"use strict"
+
+var items = {}
+module.exports = items
+
+items["keep"] = {
+	name:"keep",
+	type:"keep",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["watchTower"] = {
+	name:"watchTower",
+	type:"watchTower",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["warehouse"] = {
+	name:"warehouse",
+	type:"warehouse",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["dragonEyrie"] = {
+	name:"dragonEyrie",
+	type:"dragonEyrie",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["toolShop"] = {
+	name:"toolShop",
+	type:"toolShop",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["materialDepot"] = {
+	name:"materialDepot",
+	type:"materialDepot",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["academy"] = {
+	name:"academy",
+	type:"academy",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["barracks"] = {
+	name:"barracks",
+	type:"barracks",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["blackSmith"] = {
+	name:"blackSmith",
+	type:"blackSmith",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["foundry"] = {
+	name:"foundry",
+	type:"foundry",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["stoneMason"] = {
+	name:"stoneMason",
+	type:"stoneMason",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["lumbermill"] = {
+	name:"lumbermill",
+	type:"lumbermill",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["mill"] = {
+	name:"mill",
+	type:"mill",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["hospital"] = {
+	name:"hospital",
+	type:"hospital",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["townHall"] = {
+	name:"townHall",
+	type:"townHall",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["tradeGuild"] = {
+	name:"tradeGuild",
+	type:"tradeGuild",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["workshop"] = {
+	name:"workshop",
+	type:"workshop",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["trainingGround"] = {
+	name:"trainingGround",
+	type:"trainingGround",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["hunterHall"] = {
+	name:"hunterHall",
+	type:"hunterHall",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
+items["stable"] = {
+	name:"stable",
+	type:"stable",
+	effect:0.000000,
+	effect2:0.000000,
+	effect3:0.000000
+}
