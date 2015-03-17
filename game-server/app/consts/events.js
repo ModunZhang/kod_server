3d6f0fb418c05a3aa850a74262436fc8ffcafeff
Modun Zhang <modunzhang@gmail.com>
2015-03-17 11:08:54 +0800

    bugfix

--- game-server/app/consts/events.js
+++ game-server/app/consts/events.js
@@ -7,34 +7,7 @@
 module.exports = {
 	player:{
 		onPlayerDataChanged:"onPlayerDataChanged",
-		onGetAllianceDataSuccess:"onGetAllianceDataSuccess",
-		onBuildingLevelUp:"onBuildingLevelUp",
-		onHouseLevelUp:"onHouseLevelUp",
-		onMakeMaterialFinished:"onMakeMaterialFinished",
-		onGetMaterialSuccess:"onGetMaterialSuccess",
-		onRecruitSoldierSuccess:"onRecruitSoldierSuccess",
-		onMakeDragonEquipmentSuccess:"onMakeDragonEquipmentSuccess",
-		onTreatSoldierSuccess:"onTreatSoldierSuccess",
-		onHatchDragonSuccess:"onHatchDragonSuccess",
-		onImposeSuccess:"onImposeSuccess",
-		onGetPlayerInfoSuccess:"onGetPlayerInfoSuccess",
-		onGetNearedAllianceInfosSuccess:"onGetNearedAllianceInfosSuccess",
-		onSearchAllianceInfoByTagSuccess:"onSearchAllianceInfoByTagSuccess",
-		onSearchAlliancesSuccess:"onSearchAlliancesSuccess",
-		onGetCanDirectJoinAlliancesSuccess:"onGetCanDirectJoinAlliancesSuccess",
-		onGetMailsSuccess:"onGetMailsSuccess",
-		onGetSendMailsSuccess:"onGetSendMailsSuccess",
-		onGetSavedMailsSuccess:"onGetSavedMailsSuccess",
-		onGetReportsSuccess:"onGetReportsSuccess",
-		onGetSavedReportsSuccess:"onGetSavedReportsSuccess",
-		onGetAllianceViewDataSuccess:"onGetAllianceViewDataSuccess",
-		onGetPlayerViewDataSuccess:"onGetPlayerViewDataSuccess",
-		onGetSellItemsSuccess:"onGetSellItemsSuccess",
-		onGetAttackMarchEventDetail:"onGetAttackMarchEventDetail",
-		onGetStrikeMarchEventDetail:"onGetStrikeMarchEventDetail",
-		onGetHelpDefenceMarchEventDetail:"onGetHelpDefenceMarchEventDetail",
-		onGetHelpDefenceTroopDetail:"onGetHelpDefenceTroopDetail",
-		onAddPlayerBillingDataSuccess:"onAddPlayerBillingDataSuccess"
+		onGetAllianceDataSuccess:"onGetAllianceDataSuccess"
 	},
 	alliance:{
 		onAllianceDataChanged:"onAllianceDataChanged"
