645a3709561767d12d98a7ea9d5abc02f643d8a3
Modun Zhang <modunzhang@gmail.com>
2015-03-20 17:26:02 +0800

    bugfix

--- game-server/app/utils/marchUtils.js
+++ game-server/app/utils/marchUtils.js
@@ -242,7 +242,7 @@ var getPlayerSoldiersMarchTime = function(playerDoc, soldiers, fromAllianceDoc, 
 	var vipBuff = Vip.level[playerDoc.vipEvents.length > 0 ? DataUtils.getPlayerVipLevel(playerDoc) : 0].marchSpeedAdd
 	var time = Math.ceil(totalSpeed / totalCitizen * distance * 1000)
 	time = LogicUtils.getTimeEfffect(time, itemBuff + vipBuff)
-	return time//5 * 1000
+	return 5 * 1000
 }
 
 /**
@@ -260,7 +260,7 @@ var getPlayerDragonMarchTime = function(playerDoc, dragon, fromAllianceDoc, from
 	var baseSpeed = 2400
 	var marchSpeed = PlayerInitData.intInit.dragonMarchSpeed.value
 	var time = Math.ceil(baseSpeed / marchSpeed * distance * 1000)
-	return time//5 * 1000
+	return 5 * 1000
 }
 
 
