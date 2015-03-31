cae5f1960229876209f7071b75f4aa78fbf8dc62
Modun Zhang <modunzhang@gmail.com>
2015-03-31 15:12:44 +0800

    bugfix

--- game-server/app/datas/AllianceInitData_buildings.js
+++ game-server/app/datas/AllianceInitData_buildings.js
@@ -453,3 +453,981 @@ buildings[74] = {
 	locationX:39,
 	locationY:39
 }
+buildings[75] = {
+	index:75,
+	name:"tree",
+	locationX:2,
+	locationY:1
+}
+buildings[76] = {
+	index:76,
+	name:"tree",
+	locationX:3,
+	locationY:1
+}
+buildings[77] = {
+	index:77,
+	name:"tree",
+	locationX:17,
+	locationY:1
+}
+buildings[78] = {
+	index:78,
+	name:"tree",
+	locationX:18,
+	locationY:1
+}
+buildings[79] = {
+	index:79,
+	name:"tree",
+	locationX:19,
+	locationY:1
+}
+buildings[80] = {
+	index:80,
+	name:"tree",
+	locationX:20,
+	locationY:1
+}
+buildings[81] = {
+	index:81,
+	name:"tree",
+	locationX:23,
+	locationY:1
+}
+buildings[82] = {
+	index:82,
+	name:"tree",
+	locationX:39,
+	locationY:1
+}
+buildings[83] = {
+	index:83,
+	name:"tree",
+	locationX:41,
+	locationY:1
+}
+buildings[84] = {
+	index:84,
+	name:"tree",
+	locationX:1,
+	locationY:2
+}
+buildings[85] = {
+	index:85,
+	name:"tree",
+	locationX:21,
+	locationY:2
+}
+buildings[86] = {
+	index:86,
+	name:"tree",
+	locationX:22,
+	locationY:2
+}
+buildings[87] = {
+	index:87,
+	name:"tree",
+	locationX:23,
+	locationY:2
+}
+buildings[88] = {
+	index:88,
+	name:"tree",
+	locationX:25,
+	locationY:2
+}
+buildings[89] = {
+	index:89,
+	name:"tree",
+	locationX:40,
+	locationY:2
+}
+buildings[90] = {
+	index:90,
+	name:"tree",
+	locationX:1,
+	locationY:3
+}
+buildings[91] = {
+	index:91,
+	name:"tree",
+	locationX:21,
+	locationY:3
+}
+buildings[92] = {
+	index:92,
+	name:"tree",
+	locationX:23,
+	locationY:3
+}
+buildings[93] = {
+	index:93,
+	name:"tree",
+	locationX:1,
+	locationY:4
+}
+buildings[94] = {
+	index:94,
+	name:"tree",
+	locationX:21,
+	locationY:4
+}
+buildings[95] = {
+	index:95,
+	name:"tree",
+	locationX:22,
+	locationY:4
+}
+buildings[96] = {
+	index:96,
+	name:"tree",
+	locationX:23,
+	locationY:4
+}
+buildings[97] = {
+	index:97,
+	name:"tree",
+	locationX:41,
+	locationY:4
+}
+buildings[98] = {
+	index:98,
+	name:"tree",
+	locationX:20,
+	locationY:5
+}
+buildings[99] = {
+	index:99,
+	name:"tree",
+	locationX:21,
+	locationY:5
+}
+buildings[100] = {
+	index:100,
+	name:"tree",
+	locationX:22,
+	locationY:5
+}
+buildings[101] = {
+	index:101,
+	name:"tree",
+	locationX:41,
+	locationY:5
+}
+buildings[102] = {
+	index:102,
+	name:"tree",
+	locationX:19,
+	locationY:6
+}
+buildings[103] = {
+	index:103,
+	name:"tree",
+	locationX:20,
+	locationY:6
+}
+buildings[104] = {
+	index:104,
+	name:"tree",
+	locationX:21,
+	locationY:6
+}
+buildings[105] = {
+	index:105,
+	name:"tree",
+	locationX:20,
+	locationY:7
+}
+buildings[106] = {
+	index:106,
+	name:"tree",
+	locationX:21,
+	locationY:7
+}
+buildings[107] = {
+	index:107,
+	name:"tree",
+	locationX:20,
+	locationY:8
+}
+buildings[108] = {
+	index:108,
+	name:"tree",
+	locationX:19,
+	locationY:12
+}
+buildings[109] = {
+	index:109,
+	name:"tree",
+	locationX:17,
+	locationY:14
+}
+buildings[110] = {
+	index:110,
+	name:"tree",
+	locationX:18,
+	locationY:14
+}
+buildings[111] = {
+	index:111,
+	name:"tree",
+	locationX:17,
+	locationY:15
+}
+buildings[112] = {
+	index:112,
+	name:"tree",
+	locationX:18,
+	locationY:15
+}
+buildings[113] = {
+	index:113,
+	name:"tree",
+	locationX:22,
+	locationY:16
+}
+buildings[114] = {
+	index:114,
+	name:"tree",
+	locationX:2,
+	locationY:17
+}
+buildings[115] = {
+	index:115,
+	name:"tree",
+	locationX:21,
+	locationY:17
+}
+buildings[116] = {
+	index:116,
+	name:"tree",
+	locationX:22,
+	locationY:17
+}
+buildings[117] = {
+	index:117,
+	name:"tree",
+	locationX:41,
+	locationY:17
+}
+buildings[118] = {
+	index:118,
+	name:"tree",
+	locationX:2,
+	locationY:18
+}
+buildings[119] = {
+	index:119,
+	name:"tree",
+	locationX:3,
+	locationY:18
+}
+buildings[120] = {
+	index:120,
+	name:"tree",
+	locationX:4,
+	locationY:18
+}
+buildings[121] = {
+	index:121,
+	name:"tree",
+	locationX:5,
+	locationY:18
+}
+buildings[122] = {
+	index:122,
+	name:"tree",
+	locationX:30,
+	locationY:18
+}
+buildings[123] = {
+	index:123,
+	name:"tree",
+	locationX:31,
+	locationY:18
+}
+buildings[124] = {
+	index:124,
+	name:"tree",
+	locationX:39,
+	locationY:18
+}
+buildings[125] = {
+	index:125,
+	name:"tree",
+	locationX:40,
+	locationY:18
+}
+buildings[126] = {
+	index:126,
+	name:"tree",
+	locationX:1,
+	locationY:19
+}
+buildings[127] = {
+	index:127,
+	name:"tree",
+	locationX:2,
+	locationY:19
+}
+buildings[128] = {
+	index:128,
+	name:"tree",
+	locationX:3,
+	locationY:19
+}
+buildings[129] = {
+	index:129,
+	name:"tree",
+	locationX:4,
+	locationY:19
+}
+buildings[130] = {
+	index:130,
+	name:"tree",
+	locationX:5,
+	locationY:19
+}
+buildings[131] = {
+	index:131,
+	name:"tree",
+	locationX:6,
+	locationY:19
+}
+buildings[132] = {
+	index:132,
+	name:"tree",
+	locationX:10,
+	locationY:19
+}
+buildings[133] = {
+	index:133,
+	name:"tree",
+	locationX:12,
+	locationY:19
+}
+buildings[134] = {
+	index:134,
+	name:"tree",
+	locationX:13,
+	locationY:19
+}
+buildings[135] = {
+	index:135,
+	name:"tree",
+	locationX:14,
+	locationY:19
+}
+buildings[136] = {
+	index:136,
+	name:"tree",
+	locationX:29,
+	locationY:19
+}
+buildings[137] = {
+	index:137,
+	name:"tree",
+	locationX:30,
+	locationY:19
+}
+buildings[138] = {
+	index:138,
+	name:"tree",
+	locationX:31,
+	locationY:19
+}
+buildings[139] = {
+	index:139,
+	name:"tree",
+	locationX:37,
+	locationY:19
+}
+buildings[140] = {
+	index:140,
+	name:"tree",
+	locationX:38,
+	locationY:19
+}
+buildings[141] = {
+	index:141,
+	name:"tree",
+	locationX:39,
+	locationY:19
+}
+buildings[142] = {
+	index:142,
+	name:"tree",
+	locationX:41,
+	locationY:19
+}
+buildings[143] = {
+	index:143,
+	name:"tree",
+	locationX:1,
+	locationY:20
+}
+buildings[144] = {
+	index:144,
+	name:"tree",
+	locationX:2,
+	locationY:20
+}
+buildings[145] = {
+	index:145,
+	name:"tree",
+	locationX:6,
+	locationY:20
+}
+buildings[146] = {
+	index:146,
+	name:"tree",
+	locationX:7,
+	locationY:20
+}
+buildings[147] = {
+	index:147,
+	name:"tree",
+	locationX:8,
+	locationY:20
+}
+buildings[148] = {
+	index:148,
+	name:"tree",
+	locationX:9,
+	locationY:20
+}
+buildings[149] = {
+	index:149,
+	name:"tree",
+	locationX:10,
+	locationY:20
+}
+buildings[150] = {
+	index:150,
+	name:"tree",
+	locationX:11,
+	locationY:20
+}
+buildings[151] = {
+	index:151,
+	name:"tree",
+	locationX:12,
+	locationY:20
+}
+buildings[152] = {
+	index:152,
+	name:"tree",
+	locationX:13,
+	locationY:20
+}
+buildings[153] = {
+	index:153,
+	name:"tree",
+	locationX:14,
+	locationY:20
+}
+buildings[154] = {
+	index:154,
+	name:"tree",
+	locationX:15,
+	locationY:20
+}
+buildings[155] = {
+	index:155,
+	name:"tree",
+	locationX:23,
+	locationY:20
+}
+buildings[156] = {
+	index:156,
+	name:"tree",
+	locationX:24,
+	locationY:20
+}
+buildings[157] = {
+	index:157,
+	name:"tree",
+	locationX:31,
+	locationY:20
+}
+buildings[158] = {
+	index:158,
+	name:"tree",
+	locationX:32,
+	locationY:20
+}
+buildings[159] = {
+	index:159,
+	name:"tree",
+	locationX:36,
+	locationY:20
+}
+buildings[160] = {
+	index:160,
+	name:"tree",
+	locationX:37,
+	locationY:20
+}
+buildings[161] = {
+	index:161,
+	name:"tree",
+	locationX:38,
+	locationY:20
+}
+buildings[162] = {
+	index:162,
+	name:"tree",
+	locationX:41,
+	locationY:20
+}
+buildings[163] = {
+	index:163,
+	name:"tree",
+	locationX:2,
+	locationY:21
+}
+buildings[164] = {
+	index:164,
+	name:"tree",
+	locationX:6,
+	locationY:21
+}
+buildings[165] = {
+	index:165,
+	name:"tree",
+	locationX:7,
+	locationY:21
+}
+buildings[166] = {
+	index:166,
+	name:"tree",
+	locationX:8,
+	locationY:21
+}
+buildings[167] = {
+	index:167,
+	name:"tree",
+	locationX:10,
+	locationY:21
+}
+buildings[168] = {
+	index:168,
+	name:"tree",
+	locationX:11,
+	locationY:21
+}
+buildings[169] = {
+	index:169,
+	name:"tree",
+	locationX:13,
+	locationY:21
+}
+buildings[170] = {
+	index:170,
+	name:"tree",
+	locationX:14,
+	locationY:21
+}
+buildings[171] = {
+	index:171,
+	name:"tree",
+	locationX:23,
+	locationY:21
+}
+buildings[172] = {
+	index:172,
+	name:"tree",
+	locationX:33,
+	locationY:21
+}
+buildings[173] = {
+	index:173,
+	name:"tree",
+	locationX:34,
+	locationY:21
+}
+buildings[174] = {
+	index:174,
+	name:"tree",
+	locationX:35,
+	locationY:21
+}
+buildings[175] = {
+	index:175,
+	name:"tree",
+	locationX:36,
+	locationY:21
+}
+buildings[176] = {
+	index:176,
+	name:"tree",
+	locationX:38,
+	locationY:21
+}
+buildings[177] = {
+	index:177,
+	name:"tree",
+	locationX:39,
+	locationY:21
+}
+buildings[178] = {
+	index:178,
+	name:"tree",
+	locationX:40,
+	locationY:21
+}
+buildings[179] = {
+	index:179,
+	name:"tree",
+	locationX:41,
+	locationY:21
+}
+buildings[180] = {
+	index:180,
+	name:"tree",
+	locationX:2,
+	locationY:22
+}
+buildings[181] = {
+	index:181,
+	name:"tree",
+	locationX:6,
+	locationY:22
+}
+buildings[182] = {
+	index:182,
+	name:"tree",
+	locationX:7,
+	locationY:22
+}
+buildings[183] = {
+	index:183,
+	name:"tree",
+	locationX:35,
+	locationY:22
+}
+buildings[184] = {
+	index:184,
+	name:"tree",
+	locationX:36,
+	locationY:22
+}
+buildings[185] = {
+	index:185,
+	name:"tree",
+	locationX:37,
+	locationY:22
+}
+buildings[186] = {
+	index:186,
+	name:"tree",
+	locationX:41,
+	locationY:22
+}
+buildings[187] = {
+	index:187,
+	name:"tree",
+	locationX:1,
+	locationY:23
+}
+buildings[188] = {
+	index:188,
+	name:"tree",
+	locationX:2,
+	locationY:23
+}
+buildings[189] = {
+	index:189,
+	name:"tree",
+	locationX:6,
+	locationY:23
+}
+buildings[190] = {
+	index:190,
+	name:"tree",
+	locationX:18,
+	locationY:23
+}
+buildings[191] = {
+	index:191,
+	name:"tree",
+	locationX:19,
+	locationY:23
+}
+buildings[192] = {
+	index:192,
+	name:"tree",
+	locationX:40,
+	locationY:23
+}
+buildings[193] = {
+	index:193,
+	name:"tree",
+	locationX:41,
+	locationY:23
+}
+buildings[194] = {
+	index:194,
+	name:"tree",
+	locationX:1,
+	locationY:24
+}
+buildings[195] = {
+	index:195,
+	name:"tree",
+	locationX:18,
+	locationY:24
+}
+buildings[196] = {
+	index:196,
+	name:"tree",
+	locationX:39,
+	locationY:24
+}
+buildings[197] = {
+	index:197,
+	name:"tree",
+	locationX:32,
+	locationY:26
+}
+buildings[198] = {
+	index:198,
+	name:"tree",
+	locationX:32,
+	locationY:27
+}
+buildings[199] = {
+	index:199,
+	name:"tree",
+	locationX:26,
+	locationY:29
+}
+buildings[200] = {
+	index:200,
+	name:"tree",
+	locationX:27,
+	locationY:29
+}
+buildings[201] = {
+	index:201,
+	name:"tree",
+	locationX:28,
+	locationY:29
+}
+buildings[202] = {
+	index:202,
+	name:"tree",
+	locationX:27,
+	locationY:30
+}
+buildings[203] = {
+	index:203,
+	name:"tree",
+	locationX:19,
+	locationY:32
+}
+buildings[204] = {
+	index:204,
+	name:"tree",
+	locationX:20,
+	locationY:32
+}
+buildings[205] = {
+	index:205,
+	name:"tree",
+	locationX:17,
+	locationY:33
+}
+buildings[206] = {
+	index:206,
+	name:"tree",
+	locationX:18,
+	locationY:33
+}
+buildings[207] = {
+	index:207,
+	name:"tree",
+	locationX:19,
+	locationY:33
+}
+buildings[208] = {
+	index:208,
+	name:"tree",
+	locationX:20,
+	locationY:33
+}
+buildings[209] = {
+	index:209,
+	name:"tree",
+	locationX:17,
+	locationY:34
+}
+buildings[210] = {
+	index:210,
+	name:"tree",
+	locationX:20,
+	locationY:34
+}
+buildings[211] = {
+	index:211,
+	name:"tree",
+	locationX:21,
+	locationY:34
+}
+buildings[212] = {
+	index:212,
+	name:"tree",
+	locationX:17,
+	locationY:35
+}
+buildings[213] = {
+	index:213,
+	name:"tree",
+	locationX:20,
+	locationY:35
+}
+buildings[214] = {
+	index:214,
+	name:"tree",
+	locationX:21,
+	locationY:35
+}
+buildings[215] = {
+	index:215,
+	name:"tree",
+	locationX:21,
+	locationY:36
+}
+buildings[216] = {
+	index:216,
+	name:"tree",
+	locationX:22,
+	locationY:36
+}
+buildings[217] = {
+	index:217,
+	name:"tree",
+	locationX:23,
+	locationY:36
+}
+buildings[218] = {
+	index:218,
+	name:"tree",
+	locationX:1,
+	locationY:37
+}
+buildings[219] = {
+	index:219,
+	name:"tree",
+	locationX:1,
+	locationY:38
+}
+buildings[220] = {
+	index:220,
+	name:"tree",
+	locationX:24,
+	locationY:38
+}
+buildings[221] = {
+	index:221,
+	name:"tree",
+	locationX:40,
+	locationY:38
+}
+buildings[222] = {
+	index:222,
+	name:"tree",
+	locationX:41,
+	locationY:39
+}
+buildings[223] = {
+	index:223,
+	name:"tree",
+	locationX:1,
+	locationY:40
+}
+buildings[224] = {
+	index:224,
+	name:"tree",
+	locationX:3,
+	locationY:40
+}
+buildings[225] = {
+	index:225,
+	name:"tree",
+	locationX:17,
+	locationY:40
+}
+buildings[226] = {
+	index:226,
+	name:"tree",
+	locationX:21,
+	locationY:40
+}
+buildings[227] = {
+	index:227,
+	name:"tree",
+	locationX:22,
+	locationY:40
+}
+buildings[228] = {
+	index:228,
+	name:"tree",
+	locationX:24,
+	locationY:40
+}
+buildings[229] = {
+	index:229,
+	name:"tree",
+	locationX:41,
+	locationY:40
+}
+buildings[230] = {
+	index:230,
+	name:"tree",
+	locationX:4,
+	locationY:41
+}
+buildings[231] = {
+	index:231,
+	name:"tree",
+	locationX:18,
+	locationY:41
+}
+buildings[232] = {
+	index:232,
+	name:"tree",
+	locationX:19,
+	locationY:41
+}
+buildings[233] = {
+	index:233,
+	name:"tree",
+	locationX:20,
+	locationY:41
+}
+buildings[234] = {
+	index:234,
+	name:"tree",
+	locationX:21,
+	locationY:41
+}
+buildings[235] = {
+	index:235,
+	name:"tree",
+	locationX:22,
+	locationY:41
+}
+buildings[236] = {
+	index:236,
+	name:"tree",
+	locationX:37,
+	locationY:41
+}
+buildings[237] = {
+	index:237,
+	name:"tree",
+	locationX:39,
+	locationY:41
+}
