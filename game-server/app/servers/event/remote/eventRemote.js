bcd881d0423f48033148dbc8ed6959fdf8e7b792
Modun Zhang <modunzhang@gmail.com>
2015-04-21 19:50:28 +0800

    bugfix

--- game-server/app/servers/event/remote/eventRemote.js
+++ game-server/app/servers/event/remote/eventRemote.js
@@ -28,6 +28,7 @@ var pro = EventRemote.prototype
  * @param callback
  */
 pro.addToAllianceChannel = function(allianceId, uid, logicServerId, callback){
+	console.log(this.app.get("eventServerId"), "111111111111")
 	this.channelService.getChannel(Consts.AllianceChannelPrefix + "_" + allianceId, true).add(uid, logicServerId)
 	callback()
 }
@@ -40,6 +41,7 @@ pro.addToAllianceChannel = function(allianceId, uid, logicServerId, callback){
  * @param callback
  */
 pro.removeFromAllianceChannel = function(allianceId, uid, logicServerId, callback){
+	console.log(this.app.get("eventServerId"), "2222222222222")
 	var channel = this.channelService.getChannel(Consts.AllianceChannelPrefix + "_" + allianceId, false)
 	channel.leave(uid, logicServerId)
 	if(channel.getMembers.length == 0) channel.destroy()
