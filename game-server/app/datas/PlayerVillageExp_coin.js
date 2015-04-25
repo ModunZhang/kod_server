1a431bae28dadddf1d36d68230dd66b4f8f7b3c7
Modun Zhang <modunzhang@gmail.com>
2015-04-25 16:35:52 +0800

    modify configs

--- game-server/app/datas/PlayerVillageExp_coin.js
+++ game-server/app/datas/PlayerVillageExp_coin.js
@@ -7,59 +7,59 @@ coin[1] = {
 	level:1,
 	expFrom:0,
 	expTo:50,
-	percentPerHour:0.150000
+	percentPerHour:0.420000
 }
 coin[2] = {
 	level:2,
 	expFrom:51,
 	expTo:214,
-	percentPerHour:0.160000
+	percentPerHour:0.440000
 }
 coin[3] = {
 	level:3,
 	expFrom:215,
 	expTo:1769,
-	percentPerHour:0.170000
+	percentPerHour:0.460000
 }
 coin[4] = {
 	level:4,
 	expFrom:1770,
 	expTo:7607,
-	percentPerHour:0.180000
+	percentPerHour:0.480000
 }
 coin[5] = {
 	level:5,
 	expFrom:7608,
 	expTo:20387,
-	percentPerHour:0.190000
+	percentPerHour:0.500000
 }
 coin[6] = {
 	level:6,
 	expFrom:20388,
 	expTo:48748,
-	percentPerHour:0.200000
+	percentPerHour:0.520000
 }
 coin[7] = {
 	level:7,
 	expFrom:48749,
 	expTo:104863,
-	percentPerHour:0.210000
+	percentPerHour:0.540000
 }
 coin[8] = {
 	level:8,
 	expFrom:104864,
 	expTo:217010,
-	percentPerHour:0.220000
+	percentPerHour:0.560000
 }
 coin[9] = {
 	level:9,
 	expFrom:217011,
 	expTo:419315,
-	percentPerHour:0.230000
+	percentPerHour:0.580000
 }
 coin[10] = {
 	level:10,
 	expFrom:419316,
 	expTo:786515,
-	percentPerHour:0.240000
+	percentPerHour:0.600000
 }
