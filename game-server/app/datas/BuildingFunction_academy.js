adf38fc0f0bf9496098fe4009d56e41aede8ecc6
Modun Zhang <modunzhang@gmail.com>
2015-04-29 17:46:46 +0800

    modify configs

--- game-server/app/datas/BuildingFunction_academy.js
+++ game-server/app/datas/BuildingFunction_academy.js
@@ -5,201 +5,201 @@ module.exports = academy
 
 academy[1] = {
 	level:1,
-	efficiency:0.010000,
+	efficiency:0.020000,
 	power:42
 }
 academy[2] = {
 	level:2,
-	efficiency:0.020000,
+	efficiency:0.040000,
 	power:85
 }
 academy[3] = {
 	level:3,
-	efficiency:0.030000,
+	efficiency:0.060000,
 	power:127
 }
 academy[4] = {
 	level:4,
-	efficiency:0.040000,
+	efficiency:0.080000,
 	power:170
 }
 academy[5] = {
 	level:5,
-	efficiency:0.050000,
+	efficiency:0.100000,
 	power:212
 }
 academy[6] = {
 	level:6,
-	efficiency:0.060000,
+	efficiency:0.120000,
 	power:255
 }
 academy[7] = {
 	level:7,
-	efficiency:0.070000,
+	efficiency:0.140000,
 	power:297
 }
 academy[8] = {
 	level:8,
-	efficiency:0.080000,
+	efficiency:0.160000,
 	power:340
 }
 academy[9] = {
 	level:9,
-	efficiency:0.090000,
+	efficiency:0.180000,
 	power:382
 }
 academy[10] = {
 	level:10,
-	efficiency:0.100000,
+	efficiency:0.200000,
 	power:425
 }
 academy[11] = {
 	level:11,
-	efficiency:0.110000,
+	efficiency:0.220000,
 	power:637
 }
 academy[12] = {
 	level:12,
-	efficiency:0.120000,
+	efficiency:0.240000,
 	power:850
 }
 academy[13] = {
 	level:13,
-	efficiency:0.130000,
+	efficiency:0.260000,
 	power:1275
 }
 academy[14] = {
 	level:14,
-	efficiency:0.140000,
+	efficiency:0.280000,
 	power:1700
 }
 academy[15] = {
 	level:15,
-	efficiency:0.150000,
+	efficiency:0.300000,
 	power:2550
 }
 academy[16] = {
 	level:16,
-	efficiency:0.160000,
+	efficiency:0.320000,
 	power:3400
 }
 academy[17] = {
 	level:17,
-	efficiency:0.170000,
+	efficiency:0.340000,
 	power:4250
 }
 academy[18] = {
 	level:18,
-	efficiency:0.180000,
+	efficiency:0.360000,
 	power:5100
 }
 academy[19] = {
 	level:19,
-	efficiency:0.190000,
+	efficiency:0.380000,
 	power:5950
 }
 academy[20] = {
 	level:20,
-	efficiency:0.200000,
+	efficiency:0.400000,
 	power:6800
 }
 academy[21] = {
 	level:21,
-	efficiency:0.210000,
+	efficiency:0.420000,
 	power:7650
 }
 academy[22] = {
 	level:22,
-	efficiency:0.220000,
+	efficiency:0.440000,
 	power:11475
 }
 academy[23] = {
 	level:23,
-	efficiency:0.230000,
+	efficiency:0.460000,
 	power:15300
 }
 academy[24] = {
 	level:24,
-	efficiency:0.240000,
+	efficiency:0.480000,
 	power:19125
 }
 academy[25] = {
 	level:25,
-	efficiency:0.250000,
+	efficiency:0.500000,
 	power:25500
 }
 academy[26] = {
 	level:26,
-	efficiency:0.260000,
+	efficiency:0.520000,
 	power:38250
 }
 academy[27] = {
 	level:27,
-	efficiency:0.270000,
+	efficiency:0.540000,
 	power:51000
 }
 academy[28] = {
 	level:28,
-	efficiency:0.280000,
+	efficiency:0.560000,
 	power:68000
 }
 academy[29] = {
 	level:29,
-	efficiency:0.290000,
+	efficiency:0.580000,
 	power:85000
 }
 academy[30] = {
 	level:30,
-	efficiency:0.300000,
+	efficiency:0.600000,
 	power:102000
 }
 academy[31] = {
 	level:31,
-	efficiency:0.310000,
+	efficiency:0.620000,
 	power:119000
 }
 academy[32] = {
 	level:32,
-	efficiency:0.320000,
+	efficiency:0.640000,
 	power:136000
 }
 academy[33] = {
 	level:33,
-	efficiency:0.330000,
+	efficiency:0.660000,
 	power:153000
 }
 academy[34] = {
 	level:34,
-	efficiency:0.340000,
+	efficiency:0.680000,
 	power:170000
 }
 academy[35] = {
 	level:35,
-	efficiency:0.350000,
+	efficiency:0.700000,
 	power:187000
 }
 academy[36] = {
 	level:36,
-	efficiency:0.360000,
+	efficiency:0.720000,
 	power:212500
 }
 academy[37] = {
 	level:37,
-	efficiency:0.370000,
+	efficiency:0.740000,
 	power:238000
 }
 academy[38] = {
 	level:38,
-	efficiency:0.380000,
+	efficiency:0.760000,
 	power:263500
 }
 academy[39] = {
 	level:39,
-	efficiency:0.390000,
+	efficiency:0.780000,
 	power:289000
 }
 academy[40] = {
 	level:40,
-	efficiency:0.400000,
+	efficiency:0.800000,
 	power:382500
 }
