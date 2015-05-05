40bdc2f9c85add99dcd88ee8af41f264f2abf85b
Modun Zhang <modunzhang@gmail.com>
2015-05-05 19:52:59 +0800

    debug

--- game-server/app/utils/dataUtils.js
+++ game-server/app/utils/dataUtils.js
@@ -2231,6 +2231,7 @@ Utils.createSoldiersForFight = function(soldiers){
 	soldiersForFight = _.sortBy(soldiersForFight, function(soldier){
 		return -(soldier.power * soldier.totalCount)
 	})
+	console.log(soldiersForFight, "1111111111")
 	return soldiersForFight
 }
 
@@ -2252,6 +2253,7 @@ Utils.createPlayerDragonForFight = function(playerDoc, dragon, terrain){
 		currentHp:dragon.hp,
 		isWin:false
 	}
+	console.log(dragonForFight, "22222222222")
 	return dragonForFight
 }
 
