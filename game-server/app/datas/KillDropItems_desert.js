a032f4e30cd96e30afd5b1cbeda4a3c29080e592
Modun Zhang <modunzhang@gmail.com>
2015-05-12 20:29:31 +0800

    调整配置文件

--- game-server/app/datas/KillDropItems_desert.js
+++ game-server/app/datas/KillDropItems_desert.js
@@ -5,127 +5,187 @@ module.exports = desert
 
 desert[1] = {
 	level:1,
-	killScoreMin:20,
-	killScoreMax:25,
-	rewards:"dragonMaterials:ingo_1:1:30,dragonMaterials:redCrystal_1:1:40,dragonMaterials:runes_1:1:30"
+	killScoreMin:100,
+	killScoreMax:301,
+	rewards:"dragonMaterials:ingo_1:1:32,dragonMaterials:redCrystal_1:1:40,dragonMaterials:runes_1:1:28"
 }
 desert[2] = {
 	level:2,
-	killScoreMin:26,
-	killScoreMax:50,
-	rewards:"dragonMaterials:ingo_1:2:30,dragonMaterials:redCrystal_1:2:40,dragonMaterials:runes_1:2:30"
+	killScoreMin:300,
+	killScoreMax:401,
+	rewards:"dragonMaterials:ingo_1:1:28,dragonMaterials:redCrystal_1:1:37,dragonMaterials:runes_1:1:25,dragonMaterials:ingo_2:1:3,dragonMaterials:redSoul_2:1:1,dragonMaterials:redCrystal_2:1:4,dragonMaterials:runes_2:1:2"
 }
 desert[3] = {
 	level:3,
-	killScoreMin:51,
-	killScoreMax:75,
-	rewards:"dragonMaterials:ingo_1:3:30,dragonMaterials:redCrystal_1:3:40,dragonMaterials:runes_1:3:30"
+	killScoreMin:400,
+	killScoreMax:601,
+	rewards:"dragonMaterials:ingo_1:1:25,dragonMaterials:redCrystal_1:1:33,dragonMaterials:runes_1:1:22,dragonMaterials:ingo_2:1:6,dragonMaterials:redSoul_2:1:2,dragonMaterials:redCrystal_2:1:7,dragonMaterials:runes_2:1:5"
 }
 desert[4] = {
 	level:4,
-	killScoreMin:76,
-	killScoreMax:100,
-	rewards:"dragonMaterials:ingo_1:4:30,dragonMaterials:redCrystal_1:4:40,dragonMaterials:runes_1:4:30"
+	killScoreMin:600,
+	killScoreMax:701,
+	rewards:"dragonMaterials:ingo_1:1:22,dragonMaterials:redCrystal_1:1:29,dragonMaterials:runes_1:1:20,dragonMaterials:ingo_2:1:8,dragonMaterials:redSoul_2:1:3,dragonMaterials:redCrystal_2:1:11,dragonMaterials:runes_2:1:7"
 }
 desert[5] = {
 	level:5,
-	killScoreMin:101,
-	killScoreMax:200,
-	rewards:"dragonMaterials:ingo_2:1:25,dragonMaterials:redCrystal_2:1:25,dragonMaterials:runes_2:1:25,dragonMaterials:redSoul_2:1:25"
+	killScoreMin:700,
+	killScoreMax:801,
+	rewards:"dragonMaterials:ingo_1:1:18,dragonMaterials:redCrystal_1:1:25,dragonMaterials:runes_1:1:17,dragonMaterials:ingo_2:1:11,dragonMaterials:redSoul_2:1:4,dragonMaterials:redCrystal_2:1:15,dragonMaterials:runes_2:1:10"
 }
 desert[6] = {
 	level:6,
-	killScoreMin:201,
-	killScoreMax:300,
-	rewards:"dragonMaterials:ingo_2:2:25,dragonMaterials:redCrystal_2:2:25,dragonMaterials:runes_2:2:25,dragonMaterials:redSoul_2:2:25"
+	killScoreMin:800,
+	killScoreMax:901,
+	rewards:"dragonMaterials:ingo_1:1:16,dragonMaterials:redCrystal_1:1:21,dragonMaterials:runes_1:1:14,dragonMaterials:ingo_2:1:14,dragonMaterials:redSoul_2:1:5,dragonMaterials:redCrystal_2:1:18,dragonMaterials:runes_2:1:12"
 }
 desert[7] = {
 	level:7,
-	killScoreMin:301,
-	killScoreMax:400,
-	rewards:"dragonMaterials:ingo_2:3:25,dragonMaterials:redCrystal_2:3:25,dragonMaterials:runes_2:3:25,dragonMaterials:redSoul_2:3:25"
+	killScoreMin:900,
+	killScoreMax:1001,
+	rewards:"dragonMaterials:ingo_1:1:12,dragonMaterials:redCrystal_1:1:16,dragonMaterials:runes_1:1:12,dragonMaterials:ingo_2:1:17,dragonMaterials:redSoul_2:1:6,dragonMaterials:redCrystal_2:1:22,dragonMaterials:runes_2:1:15"
 }
 desert[8] = {
 	level:8,
-	killScoreMin:401,
-	killScoreMax:500,
-	rewards:"dragonMaterials:ingo_2:4:25,dragonMaterials:redCrystal_2:4:25,dragonMaterials:runes_2:4:25,dragonMaterials:redSoul_2:4:25"
+	killScoreMin:1000,
+	killScoreMax:1101,
+	rewards:"dragonMaterials:ingo_1:1:9,dragonMaterials:redCrystal_1:1:12,dragonMaterials:runes_1:1:8,dragonMaterials:ingo_2:1:20,dragonMaterials:redSoul_2:1:8,dragonMaterials:redCrystal_2:1:25,dragonMaterials:runes_2:1:18"
 }
 desert[9] = {
 	level:9,
-	killScoreMin:501,
-	killScoreMax:800,
-	rewards:"dragonMaterials:ingo_3:1:25,dragonMaterials:redCrystal_3:1:25,dragonMaterials:runes_3:1:25,dragonMaterials:redSoul_3:1:25"
+	killScoreMin:1100,
+	killScoreMax:1301,
+	rewards:"dragonMaterials:ingo_1:1:6,dragonMaterials:redCrystal_1:1:8,dragonMaterials:runes_1:1:6,dragonMaterials:ingo_2:1:22,dragonMaterials:redSoul_2:1:9,dragonMaterials:redCrystal_2:1:29,dragonMaterials:runes_2:1:20"
 }
 desert[10] = {
 	level:10,
-	killScoreMin:801,
-	killScoreMax:1100,
-	rewards:"dragonMaterials:ingo_3:2:25,dragonMaterials:redCrystal_3:2:25,dragonMaterials:runes_3:2:25,dragonMaterials:redSoul_3:2:25"
+	killScoreMin:1300,
+	killScoreMax:2601,
+	rewards:"dragonMaterials:ingo_1:1:3,dragonMaterials:redCrystal_1:1:4,dragonMaterials:runes_1:1:3,dragonMaterials:ingo_2:1:25,dragonMaterials:redSoul_2:1:10,dragonMaterials:redCrystal_2:1:33,dragonMaterials:runes_2:1:22"
 }
 desert[11] = {
 	level:11,
-	killScoreMin:1101,
-	killScoreMax:1400,
-	rewards:"dragonMaterials:ingo_3:3:25,dragonMaterials:redCrystal_3:3:25,dragonMaterials:runes_3:3:25,dragonMaterials:redSoul_3:3:25"
+	killScoreMin:2600,
+	killScoreMax:3101,
+	rewards:"dragonMaterials:ingo_2:1:28,dragonMaterials:redSoul_2:1:11,dragonMaterials:redCrystal_2:1:36,dragonMaterials:runes_2:1:25"
 }
 desert[12] = {
 	level:12,
-	killScoreMin:1401,
-	killScoreMax:1700,
-	rewards:"dragonMaterials:ingo_3:4:25,dragonMaterials:redCrystal_3:4:25,dragonMaterials:runes_3:4:25,dragonMaterials:redSoul_3:4:25"
+	killScoreMin:3100,
+	killScoreMax:3601,
+	rewards:"dragonMaterials:ingo_2:1:25,dragonMaterials:redSoul_2:1:10,dragonMaterials:redCrystal_2:1:32,dragonMaterials:runes_2:1:22,dragonMaterials:ingo_3:1:3,dragonMaterials:redSoul_3:1:1,dragonMaterials:redCrystal_3:1:4,dragonMaterials:runes_3:1:3"
 }
 desert[13] = {
 	level:13,
-	killScoreMin:1701,
-	killScoreMax:2000,
-	rewards:"dragonMaterials:ingo_3:5:25,dragonMaterials:redCrystal_3:5:25,dragonMaterials:runes_3:5:25,dragonMaterials:redSoul_3:5:26"
+	killScoreMin:3600,
+	killScoreMax:4101,
+	rewards:"dragonMaterials:ingo_2:1:22,dragonMaterials:redSoul_2:1:9,dragonMaterials:redCrystal_2:1:29,dragonMaterials:runes_2:1:20,dragonMaterials:ingo_3:1:5,dragonMaterials:redSoul_3:1:2,dragonMaterials:redCrystal_3:1:8,dragonMaterials:runes_3:1:5"
 }
 desert[14] = {
 	level:14,
-	killScoreMin:2001,
-	killScoreMax:3000,
-	rewards:"dragonMaterials:ingo_4:1:25,dragonMaterials:redCrystal_4:1:25,dragonMaterials:runes_4:1:25,dragonMaterials:redSoul_4:1:25"
+	killScoreMin:4100,
+	killScoreMax:4601,
+	rewards:"dragonMaterials:ingo_2:1:20,dragonMaterials:redSoul_2:1:8,dragonMaterials:redCrystal_2:1:25,dragonMaterials:runes_2:1:18,dragonMaterials:ingo_3:1:8,dragonMaterials:redSoul_3:1:3,dragonMaterials:redCrystal_3:1:11,dragonMaterials:runes_3:1:8"
 }
 desert[15] = {
 	level:15,
-	killScoreMin:3001,
-	killScoreMax:4000,
-	rewards:"dragonMaterials:ingo_4:2:25,dragonMaterials:redCrystal_4:2:25,dragonMaterials:runes_4:2:25,dragonMaterials:redSoul_4:2:25"
+	killScoreMin:4600,
+	killScoreMax:5101,
+	rewards:"dragonMaterials:ingo_2:1:17,dragonMaterials:redSoul_2:1:7,dragonMaterials:redCrystal_2:1:22,dragonMaterials:runes_2:1:15,dragonMaterials:ingo_3:1:11,dragonMaterials:redSoul_3:1:4,dragonMaterials:redCrystal_3:1:14,dragonMaterials:runes_3:1:10"
 }
 desert[16] = {
 	level:16,
-	killScoreMin:4001,
-	killScoreMax:5000,
-	rewards:"dragonMaterials:ingo_4:3:25,dragonMaterials:redCrystal_4:3:25,dragonMaterials:runes_4:3:25,dragonMaterials:redSoul_4:3:25"
+	killScoreMin:5100,
+	killScoreMax:6701,
+	rewards:"dragonMaterials:ingo_2:1:14,dragonMaterials:redSoul_2:1:6,dragonMaterials:redCrystal_2:1:18,dragonMaterials:runes_2:1:12,dragonMaterials:ingo_3:1:14,dragonMaterials:redSoul_3:1:5,dragonMaterials:redCrystal_3:1:19,dragonMaterials:runes_3:1:12"
 }
 desert[17] = {
 	level:17,
-	killScoreMin:5001,
-	killScoreMax:6000,
-	rewards:"dragonMaterials:ingo_4:4:25,dragonMaterials:redCrystal_4:4:25,dragonMaterials:runes_4:4:25,dragonMaterials:redSoul_4:4:25"
+	killScoreMin:6700,
+	killScoreMax:8301,
+	rewards:"dragonMaterials:ingo_2:1:11,dragonMaterials:redSoul_2:1:4,dragonMaterials:redCrystal_2:1:14,dragonMaterials:runes_2:1:10,dragonMaterials:ingo_3:1:17,dragonMaterials:redSoul_3:1:7,dragonMaterials:redCrystal_3:1:22,dragonMaterials:runes_3:1:15"
 }
 desert[18] = {
 	level:18,
-	killScoreMin:6001,
-	killScoreMax:7000,
-	rewards:"dragonMaterials:ingo_4:5:25,dragonMaterials:redCrystal_4:5:25,dragonMaterials:runes_4:5:25,dragonMaterials:redSoul_4:5:25"
+	killScoreMin:8300,
+	killScoreMax:10801,
+	rewards:"dragonMaterials:ingo_2:1:8,dragonMaterials:redSoul_2:1:3,dragonMaterials:redCrystal_2:1:11,dragonMaterials:runes_2:1:8,dragonMaterials:ingo_3:1:20,dragonMaterials:redSoul_3:1:8,dragonMaterials:redCrystal_3:1:25,dragonMaterials:runes_3:1:17"
 }
 desert[19] = {
 	level:19,
-	killScoreMin:7001,
-	killScoreMax:8000,
-	rewards:"dragonMaterials:ingo_4:6:25,dragonMaterials:redCrystal_4:6:25,dragonMaterials:runes_4:6:25,dragonMaterials:redSoul_4:6:25"
+	killScoreMin:10800,
+	killScoreMax:12401,
+	rewards:"dragonMaterials:ingo_2:1:6,dragonMaterials:redSoul_2:1:2,dragonMaterials:redCrystal_2:1:7,dragonMaterials:runes_2:1:5,dragonMaterials:ingo_3:1:22,dragonMaterials:redSoul_3:1:9,dragonMaterials:redCrystal_3:1:29,dragonMaterials:runes_3:1:20"
 }
 desert[20] = {
 	level:20,
-	killScoreMin:8001,
-	killScoreMax:9000,
-	rewards:"dragonMaterials:ingo_4:8:25,dragonMaterials:redCrystal_4:8:25,dragonMaterials:runes_4:8:25,dragonMaterials:redSoul_4:8:25"
+	killScoreMin:12400,
+	killScoreMax:15101,
+	rewards:"dragonMaterials:ingo_2:1:3,dragonMaterials:redSoul_2:1:1,dragonMaterials:redCrystal_2:1:4,dragonMaterials:runes_2:1:2,dragonMaterials:ingo_3:1:25,dragonMaterials:redSoul_3:1:10,dragonMaterials:redCrystal_3:1:33,dragonMaterials:runes_3:1:22"
 }
 desert[21] = {
 	level:21,
-	killScoreMin:9001,
-	killScoreMax:10000,
-	rewards:"dragonMaterials:ingo_4:10:25,dragonMaterials:redCrystal_4:10:25,dragonMaterials:runes_4:10:25,dragonMaterials:redSoul_4:10:25"
+	killScoreMin:15100,
+	killScoreMax:19801,
+	rewards:"dragonMaterials:ingo_3:1:28,dragonMaterials:redSoul_3:1:11,dragonMaterials:redCrystal_3:1:36,dragonMaterials:runes_3:1:25"
+}
+desert[22] = {
+	level:22,
+	killScoreMin:19800,
+	killScoreMax:23801,
+	rewards:"dragonMaterials:ingo_3:1:25,dragonMaterials:redSoul_3:1:10,dragonMaterials:redCrystal_3:1:32,dragonMaterials:runes_3:1:22,dragonMaterials:ingo_4:1:3,dragonMaterials:redSoul_4:1:1,dragonMaterials:redCrystal_4:1:4,dragonMaterials:runes_4:1:3"
+}
+desert[23] = {
+	level:23,
+	killScoreMin:23800,
+	killScoreMax:28201,
+	rewards:"dragonMaterials:ingo_3:1:22,dragonMaterials:redSoul_3:1:9,dragonMaterials:redCrystal_3:1:29,dragonMaterials:runes_3:1:20,dragonMaterials:ingo_4:1:5,dragonMaterials:redSoul_4:1:2,dragonMaterials:redCrystal_4:1:8,dragonMaterials:runes_4:1:5"
+}
+desert[24] = {
+	level:24,
+	killScoreMin:28200,
+	killScoreMax:32801,
+	rewards:"dragonMaterials:ingo_3:1:20,dragonMaterials:redSoul_3:1:8,dragonMaterials:redCrystal_3:1:25,dragonMaterials:runes_3:1:18,dragonMaterials:ingo_4:1:8,dragonMaterials:redSoul_4:1:3,dragonMaterials:redCrystal_4:1:11,dragonMaterials:runes_4:1:8"
+}
+desert[25] = {
+	level:25,
+	killScoreMin:32800,
+	killScoreMax:37801,
+	rewards:"dragonMaterials:ingo_3:1:17,dragonMaterials:redSoul_3:1:7,dragonMaterials:redCrystal_3:1:22,dragonMaterials:runes_3:1:15,dragonMaterials:ingo_4:1:11,dragonMaterials:redSoul_4:1:4,dragonMaterials:redCrystal_4:1:14,dragonMaterials:runes_4:1:10"
+}
+desert[26] = {
+	level:26,
+	killScoreMin:37800,
+	killScoreMax:39201,
+	rewards:"dragonMaterials:ingo_3:1:14,dragonMaterials:redSoul_3:1:6,dragonMaterials:redCrystal_3:1:18,dragonMaterials:runes_3:1:12,dragonMaterials:ingo_4:1:14,dragonMaterials:redSoul_4:1:5,dragonMaterials:redCrystal_4:1:19,dragonMaterials:runes_4:1:12"
+}
+desert[27] = {
+	level:27,
+	killScoreMin:39200,
+	killScoreMax:44501,
+	rewards:"dragonMaterials:ingo_3:1:11,dragonMaterials:redSoul_3:1:4,dragonMaterials:redCrystal_3:1:14,dragonMaterials:runes_3:1:10,dragonMaterials:ingo_4:1:17,dragonMaterials:redSoul_4:1:7,dragonMaterials:redCrystal_4:1:22,dragonMaterials:runes_4:1:15"
+}
+desert[28] = {
+	level:28,
+	killScoreMin:44500,
+	killScoreMax:50001,
+	rewards:"dragonMaterials:ingo_3:1:8,dragonMaterials:redSoul_3:1:3,dragonMaterials:redCrystal_3:1:11,dragonMaterials:runes_3:1:8,dragonMaterials:ingo_4:1:20,dragonMaterials:redSoul_4:1:8,dragonMaterials:redCrystal_4:1:25,dragonMaterials:runes_4:1:17"
+}
+desert[29] = {
+	level:29,
+	killScoreMin:50000,
+	killScoreMax:60001,
+	rewards:"dragonMaterials:ingo_3:1:6,dragonMaterials:redSoul_3:1:2,dragonMaterials:redCrystal_3:1:7,dragonMaterials:runes_3:1:5,dragonMaterials:ingo_4:1:22,dragonMaterials:redSoul_4:1:9,dragonMaterials:redCrystal_4:1:29,dragonMaterials:runes_4:1:20"
+}
+desert[30] = {
+	level:30,
+	killScoreMin:60000,
+	killScoreMax:80001,
+	rewards:"dragonMaterials:ingo_3:1:3,dragonMaterials:redSoul_3:1:1,dragonMaterials:redCrystal_3:1:4,dragonMaterials:runes_3:1:2,dragonMaterials:ingo_4:1:25,dragonMaterials:redSoul_4:1:10,dragonMaterials:redCrystal_4:1:33,dragonMaterials:runes_4:1:22"
+}
+desert[31] = {
+	level:31,
+	killScoreMin:80000,
+	killScoreMax:100000,
+	rewards:"dragonMaterials:ingo_4:1:28,dragonMaterials:redSoul_4:1:11,dragonMaterials:redCrystal_4:1:36,dragonMaterials:runes_4:1:25"
 }
