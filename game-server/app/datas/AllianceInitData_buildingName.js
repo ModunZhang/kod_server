2cdf474bf8f3baa42a3e6c704bce46ae582c9c07
Modun Zhang <modunzhang@gmail.com>
2015-05-23 16:49:35 +0800

    调整配置文件

--- game-server/app/datas/AllianceInitData_buildingName.js
+++ game-server/app/datas/AllianceInitData_buildingName.js
@@ -122,13 +122,6 @@ buildingName["foodVillage"] = {
 	type:"village",
 	moveNeedHonour:100
 }
-buildingName["coinVillage"] = {
-	name:"coinVillage",
-	width:1,
-	height:1,
-	type:"village",
-	moveNeedHonour:100
-}
 buildingName["member"] = {
 	name:"member",
 	width:1,
