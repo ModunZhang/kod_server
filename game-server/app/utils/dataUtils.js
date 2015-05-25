99a744d43eaed65b82b53f5606d1b073fa2f73c0
Modun Zhang <modunzhang@gmail.com>
2015-05-25 18:02:47 +0800

    bugfix

--- game-server/app/utils/dataUtils.js
+++ game-server/app/utils/dataUtils.js
@@ -1960,7 +1960,7 @@ Utils.createAllianceShrineStageEvent = function(stageName){
 		id:ShortId.generate(),
 		stageName:stageName,
 		createTime:Date.now(),
-		startTime:Date.now() + this.getAllianceIntInit("activeShrineStageEventMinutes") * 60 * 1000,
+		startTime:Date.now() + this.getAllianceIntInit("activeShrineStageEventMinutes") * 5 * 1000,
 		playerTroops:[]
 	}
 	return event
