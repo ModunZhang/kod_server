aea43e1c97753eb25001b7f5324a3a7324e66bf2
Modun Zhang <modunzhang@gmail.com>
2015-05-27 21:28:30 +0800

    调整服务器本地化配置

--- game-server/app/datas/Localizations_alliance.js
+++ game-server/app/datas/Localizations_alliance.js
@@ -6,110 +6,132 @@ module.exports = alliance
 alliance["RequestRejectedTitle"] = {
 	key:"RequestRejectedTitle",
 	cn:"联盟申请被拒绝",
-	en:"这是英文:联盟申请被拒绝"
+	en:"这是英文:联盟申请被拒绝",
+	tw:"聯盟申請被拒絕"
 }
 alliance["RequestRejectedContent"] = {
 	key:"RequestRejectedContent",
-	cn:"亲爱的,您对 %s 联盟的申请被无情的拒绝了",
-	en:"这是英文:亲爱的,您对 %s 联盟的申请被无情的拒绝了"
+	cn:"主人,你对 %s 联盟的申请被无情的拒绝了。",
+	en:"这是英文:主人,你对 %s 联盟的申请被无情的拒绝了。",
+	tw:"主人,你對 %s 聯盟的申請被無情的拒絕了。"
 }
 alliance["RequestApprovedTitle"] = {
 	key:"RequestApprovedTitle",
 	cn:"联盟申请审核通过",
-	en:"这是英文:联盟申请审核通过"
+	en:"这是英文:联盟申请审核通过",
+	tw:"聯盟申請審核通過"
 }
 alliance["RequestApprovedContent"] = {
 	key:"RequestApprovedContent",
-	cn:"恭喜,您对 %s 联盟的申请审核通过了",
-	en:"这是英文:恭喜,您对 %s 联盟的申请审核通过了"
+	cn:"恭喜,你对 %s 联盟的申请审核通过了。",
+	en:"这是英文:恭喜,你对 %s 联盟的申请审核通过了。",
+	tw:"恭喜,你對 %s 聯盟的申請審核通過了。"
 }
 alliance["InviteRejectedTitle"] = {
 	key:"InviteRejectedTitle",
 	cn:"加入联盟邀请被拒绝",
-	en:"这是英文:加入联盟邀请被拒绝"
+	en:"这是英文:加入联盟邀请被拒绝",
+	tw:"加入聯盟邀請被拒絕"
 }
 alliance["InviteRejectedContent"] = {
 	key:"InviteRejectedContent",
-	cn:"亲爱的,%s 拒绝了您对他加入联盟的邀请",
-	en:"这是英文:亲爱的,%s 拒绝了您对他加入联盟的邀请"
+	cn:"主人,%s 拒绝了你对他加入联盟的邀请。",
+	en:"这是英文:主人,%s 拒绝了你对他加入联盟的邀请。",
+	tw:"主人,%s 拒絕了你對他加入聯盟的邀請。"
 }
 alliance["InviteApprovedTitle"] = {
 	key:"InviteApprovedTitle",
 	cn:"加入联盟邀请被接受",
-	en:"这是英文:加入联盟邀请被接受"
+	en:"这是英文:加入联盟邀请被接受",
+	tw:"加入聯盟邀請被接受"
 }
 alliance["InviteApprovedContent"] = {
 	key:"InviteApprovedContent",
-	cn:"恭喜,%s 接受了您对他加入联盟的邀请",
-	en:"这是英文:恭喜,%s 接受了您对他加入联盟的邀请"
+	cn:"恭喜,%s 接受了你对他加入联盟的邀请。",
+	en:"这是英文:恭喜,%s 接受了你对他加入联盟的邀请。",
+	tw:"恭喜,%s 接受了你對他加入聯盟的邀請。"
 }
 alliance["HelpDefenceStrikeTitle"] = {
 	key:"HelpDefenceStrikeTitle",
-	cn:"%s<%s>帮您抵御了一次突袭",
-	en:"这是英文:%s<%s>帮您抵御了一次突袭"
+	cn:"[%s]%s帮你抵御了一次突袭",
+	en:"这是英文[%s]%s帮你抵御了一次突袭",
+	tw:"[%s]%s幫你抵禦了壹次突襲"
 }
 alliance["HelpDefenceStrikeContent"] = {
 	key:"HelpDefenceStrikeContent",
-	cn:"%s<%s>帮您抵御了一次突袭",
-	en:"这是英文:%s<%s>帮您抵御了一次突袭"
+	cn:"[%s]%s帮你抵御了一次突袭。",
+	en:"这是英文:[%s]%s帮你抵御了一次突袭。",
+	tw:"[%s]%s幫你抵禦了壹次突襲。"
 }
 alliance["HelpDefenceAttackTitle"] = {
 	key:"HelpDefenceAttackTitle",
-	cn:"%s<%s>帮您抵御了一次进攻",
-	en:"这是英文:%s<%s>帮您抵御了一次进攻"
+	cn:"[%s]%s帮你抵御了一次进攻",
+	en:"这是英文:[%s]%s帮你抵御了一次进攻",
+	tw:"[%s]%s幫你抵禦了壹次進攻"
 }
 alliance["HelpDefenceAttackContent"] = {
 	key:"HelpDefenceAttackContent",
-	cn:"%s<%s>帮您抵御了一次进攻",
-	en:"这是英文:%s<%s>帮您抵御了一次进攻"
+	cn:"[%s]%s帮你抵御了一次进攻。",
+	en:"这是英文:[%s]%s帮你抵御了一次进攻。",
+	tw:"[%s]%s幫你抵禦了壹次進攻。"
 }
 alliance["AttackAllianceMessage"] = {
 	key:"AttackAllianceMessage",
-	cn:"我方联盟刚刚发起了一次联盟战",
-	en:"这是英文:我方联盟刚刚发起了一次联盟战"
+	cn:"我方联盟刚刚发起了一场联盟会战，请做好战斗准备！",
+	en:"这是英文:我方联盟刚刚发起了一场联盟会战，，请做好战斗准备！",
+	tw:"我方聯盟剛剛發起了壹場聯盟會戰，請做好戰鬥準備！"
 }
 alliance["AllianceBeAttackedMessage"] = {
 	key:"AllianceBeAttackedMessage",
-	cn:"%s 联盟对我方联盟发起了一次联盟战",
-	en:"这是英文:%s 联盟对我方联盟发起了一次联盟战"
+	cn:"%s 联盟向我方发起了一场联盟会战，请做好战斗准备！",
+	en:"这是英文:%s 联盟对我方发起了一场联盟会战，请做好战斗准备！",
+	tw:"%s 聯盟向我方發起了壹場聯盟會戰，請做好戰鬥準備！"
 }
 alliance["AllianceTitleBeModifyedTitle"] = {
 	key:"AllianceTitleBeModifyedTitle",
-	cn:"您在联盟的职位被调整",
-	en:"这是英文:您在联盟的职位被调整"
+	cn:"你的联盟职位被调整",
+	en:"这是英文:你的联盟职位被调整",
+	tw:"你的聯盟職位被調整"
 }
 alliance["AllianceTitleBeModifyedContent"] = {
 	key:"AllianceTitleBeModifyedContent",
-	cn:"您的职位由%s调整为%s",
-	en:"这是英文:您的职位由%s调整为%s"
+	cn:"你的职位由%s调整为%s。",
+	en:"这是英文:你的职位由%s调整为%s。",
+	tw:"你的職位由%s調整為%s。"
 }
 alliance["AllianceKickMemberOffTitle"] = {
 	key:"AllianceKickMemberOffTitle",
-	cn:"您已被%s联盟踢出",
-	en:"这是英文:您已被%s联盟踢出"
+	cn:"你已被%s联盟踢出",
+	en:"这是英文:你已被%s联盟踢出",
+	tw:"你已被%s聯盟踢出"
 }
 alliance["AllianceKickMemberOffContent"] = {
 	key:"AllianceKickMemberOffContent",
-	cn:"您已被%s联盟踢出",
-	en:"这是英文:您已被%s联盟踢出"
+	cn:"你已被%s联盟踢出。",
+	en:"这是英文:你已被%s联盟踢出。",
+	tw:"你已被%s聯盟踢出。"
 }
 alliance["giveLoyaltyToAllianceMemberTitle"] = {
 	key:"giveLoyaltyToAllianceMemberTitle",
 	cn:"忠诚值奖励",
-	en:"这是英文:忠诚值奖励"
+	en:"这是英文:忠诚值奖励",
+	tw:"忠誠值獎勵"
 }
 alliance["giveLoyaltyToAllianceMemberContent"] = {
 	key:"giveLoyaltyToAllianceMemberContent",
 	cn:"%s联盟向你发放了%d点忠诚值奖励，请再接再厉！",
-	en:"这是英文:%s联盟向你发放了%d点忠诚值奖励，请再接再厉！"
+	en:"这是英文:%s联盟向你发放了%d点忠诚值奖励，请再接再厉！",
+	tw:"%s聯盟向你發放了%d點忠誠值獎勵，請再接再厲！"
 }
 alliance["AllianceFightKillFirstRewardTitle"] = {
 	key:"AllianceFightKillFirstRewardTitle",
 	cn:"联盟战最高击杀奖励",
-	en:"这是英文:联盟战最高击杀奖励"
+	en:"这是英文:联盟战最高击杀奖励",
+	tw:"聯盟戰最高擊殺獎勵"
 }
 alliance["AllianceFightKillFirstRewardContent"] = {
 	key:"AllianceFightKillFirstRewardContent",
-	cn:"由于您在联盟战中击杀数第一,系统奖励您%d金龙币",
-	en:"这是英文:由于您在联盟战中击杀数第一,系统奖励您%d金龙币"
+	cn:"你在联盟会战中奋勇杀敌，成为“头号杀手”,系统已奖励%d金龙币到你的账户中。",
+	en:"这是英文:你在联盟会战中奋勇杀敌，成为“头号杀手”,系统已奖励%d金龙币到你的账户中。",
+	tw:"你在聯盟會戰中奮勇殺敵，成為“頭號殺手”,系統已獎勵%d金龍幣到你的賬戶中。"
 }
