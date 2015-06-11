946297075d31dc590cd21c98a21089087b999b57
Modun Zhang <modunzhang@gmail.com>
2015-06-11 10:29:03 +0800

    bugfix

--- game-server/app/services/playerIAPService.js
+++ game-server/app/services/playerIAPService.js
@@ -25,7 +25,7 @@ var PlayerIAPService = function(app){
 	this.env = app.get("env")
 	this.logService = app.get("logService")
 	this.pushService = app.get("pushService")
-	this.dataService = app.get("dataService")
+	this.cacheService = app.get('cacheService');
 	this.Billing = app.get("Billing")
 	this.GemAdd = app.get("GemAdd")
 	this.billingValidateHost =  app.get('serverConfig').iapValidateUrl
@@ -150,7 +150,7 @@ var SendAllianceMembersRewardsAsync = function(senderId, senderName, memberId, r
 	var self = this
 	var memberDoc = null
 	var memberData = []
-	this.dataService.findPlayerAsync(memberId, [], true).then(function(doc){
+	this.cacheService.findPlayerAsync(memberId, [], true).then(function(doc){
 		memberDoc = doc
 		var iapGift = {
 			id:ShortId.generate(),
@@ -166,14 +166,14 @@ var SendAllianceMembersRewardsAsync = function(senderId, senderName, memberId, r
 		}
 		memberDoc.iapGifts.push(iapGift)
 		memberData.push(["iapGifts." + memberDoc.iapGifts.indexOf(iapGift), iapGift])
-		return self.dataService.updatePlayerAsync(memberDoc._id, memberDoc)
+		return self.cacheService.updatePlayerAsync(memberDoc._id, memberDoc)
 	}).then(function(){
 		return self.pushService.onPlayerDataChangedAsync(memberDoc, memberData)
 	}).catch(function(e){
 		self.logService.onEventError("logic.playerIAPService.SendAllianceMembersRewardsAsync", {senderId:senderId, memberId:memberId, reward:reward}, e.stack)
 		var funcs = []
 		if(_.isObject(memberDoc)){
-			funcs.push(self.dataService.updatePlayerAsync(memberDoc._id, null))
+			funcs.push(self.cacheService.updatePlayerAsync(memberDoc._id, null))
 		}
 		return Promise.all(funcs).then(function(){
 			return Promise.resolve()
@@ -196,7 +196,7 @@ pro.addPlayerBillingData = function(playerId, transactionId, receiptData, callba
 	var playerData = []
 	var updateFuncs = []
 	var rewards = null
-	this.dataService.findPlayerAsync(playerId, [], true).then(function(doc){
+	this.cacheService.findPlayerAsync(playerId, [], true).then(function(doc){
 		playerDoc = doc
 		return self.Billing.findOneAsync({transactionId:transactionId})
 	}).then(function(doc){
@@ -231,7 +231,7 @@ pro.addPlayerBillingData = function(playerId, transactionId, receiptData, callba
 			rewards:rewards
 		}
 		updateFuncs.push([self.GemAdd, self.GemAdd.createAsync, gemAdd])
-		updateFuncs.push([self.dataService, self.dataService.updatePlayerAsync, playerDoc._id, playerDoc])
+		updateFuncs.push([self.cacheService, self.cacheService.updatePlayerAsync, playerDoc._id, playerDoc])
 		return Promise.resolve()
 	}).then(function(){
 		return LogicUtils.excuteAll(updateFuncs)
@@ -241,7 +241,7 @@ pro.addPlayerBillingData = function(playerId, transactionId, receiptData, callba
 	}, function(e){
 		var funcs = []
 		if(_.isObject(playerDoc)){
-			funcs.push(self.dataService.updatePlayerAsync(playerDoc._id, null))
+			funcs.push(self.cacheService.updatePlayerAsync(playerDoc._id, null))
 		}
 		return Promise.all(funcs).then(function(){
 			callback(e)
@@ -249,7 +249,7 @@ pro.addPlayerBillingData = function(playerId, transactionId, receiptData, callba
 		})
 	}).then(function(){
 		if(_.isObject(rewards.rewardToAllianceMember) && !_.isEmpty(playerDoc.allianceId)){
-			return self.dataService.directFindAllianceAsync(playerDoc.allianceId, [], false).then(function(doc){
+			return self.cacheService.directFindAllianceAsync(playerDoc.allianceId, [], false).then(function(doc){
 				allianceDoc = doc
 				var funcs = []
 				_.each(allianceDoc.members, function(member){
