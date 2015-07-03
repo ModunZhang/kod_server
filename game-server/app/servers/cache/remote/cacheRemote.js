0229fe06b5264a083055a078589c0ed1c4b78e00
Modun Zhang <modunzhang@gmail.com>
2015-07-03 17:03:24 +0800

    调整服务器繁忙反应时间

--- game-server/app/servers/cache/remote/cacheRemote.js
+++ game-server/app/servers/cache/remote/cacheRemote.js
@@ -30,8 +30,8 @@ var CacheRemote = function(app){
 	this.allianceApiService3 = app.get("allianceApiService3")
 	this.allianceApiService4 = app.get("allianceApiService4")
 	this.allianceApiService5 = app.get("allianceApiService5")
-	this.toobusyMaxLag = 50
-	this.toobusyInterval = 50
+	this.toobusyMaxLag = 70
+	this.toobusyInterval = 100
 	toobusy.maxLag(this.toobusyMaxLag)
 	toobusy.interval(this.toobusyInterval)
 	this.apiMap = {}
