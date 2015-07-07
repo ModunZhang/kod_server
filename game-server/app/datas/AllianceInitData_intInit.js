8f1d97fec487d7752c5147e06b15d0a3be7a0739
Modun Zhang <modunzhang@gmail.com>
2015-07-07 21:43:22 +0800

    Revert "临时修改联盟战保护期时间"
    This reverts commit cfaad31e0bb5451d7eecdc6e4e21edd524946176.

--- game-server/app/datas/AllianceInitData_intInit.js
+++ game-server/app/datas/AllianceInitData_intInit.js
@@ -70,12 +70,12 @@ intInit["dragonStrikeHpDecreasedPercent"] = {
 }
 intInit["allianceFightFaiedProtectMinutes"] = {
 	type:"allianceFightFaiedProtectMinutes",
-	value:1,
+	value:180,
 	desc:"联盟战失败保护期时间"
 }
 intInit["allianceFightSuccessProtectMinutes"] = {
 	type:"allianceFightSuccessProtectMinutes",
-	value:1,
+	value:180,
 	desc:"联盟战成功保护期时间"
 }
 intInit["KilledCitizenPerDragonExp"] = {
