c3158ac82a3732fe75ecf5f38256f03ed60c97cd
Modun Zhang <modunzhang@gmail.com>
2015-07-13 15:29:30 +0800

    添加银币村落,调整配置文件

--- game-server/app/datas/AllianceBuilding_orderHall.js
+++ game-server/app/datas/AllianceBuilding_orderHall.js
@@ -5,61 +5,61 @@ module.exports = orderHall
 
 orderHall[1] = {
 	level:1,
-	needHonour:38800,
-	villageCount:4,
+	needHonour:5200,
+	villageCount:6,
 	power:775
 }
 orderHall[2] = {
 	level:2,
-	needHonour:77600,
-	villageCount:6,
+	needHonour:10500,
+	villageCount:8,
 	power:1550
 }
 orderHall[3] = {
 	level:3,
-	needHonour:155300,
-	villageCount:8,
+	needHonour:21000,
+	villageCount:10,
 	power:3105
 }
 orderHall[4] = {
 	level:4,
-	needHonour:310700,
-	villageCount:10,
+	needHonour:42000,
+	villageCount:12,
 	power:6210
 }
 orderHall[5] = {
 	level:5,
-	needHonour:621400,
-	villageCount:12,
+	needHonour:160300,
+	villageCount:14,
 	power:12425
 }
 orderHall[6] = {
 	level:6,
-	needHonour:1242900,
-	villageCount:14,
+	needHonour:562500,
+	villageCount:16,
 	power:24855
 }
 orderHall[7] = {
 	level:7,
-	needHonour:2485900,
-	villageCount:16,
+	needHonour:1701600,
+	villageCount:18,
 	power:57480
 }
 orderHall[8] = {
 	level:8,
-	needHonour:5748100,
-	villageCount:18,
+	needHonour:3448900,
+	villageCount:20,
 	power:108185
 }
 orderHall[9] = {
 	level:9,
-	needHonour:10818600,
-	villageCount:20,
+	needHonour:6491100,
+	villageCount:22,
 	power:214095
 }
 orderHall[10] = {
 	level:10,
-	needHonour:21409600,
-	villageCount:22,
+	needHonour:12845800,
+	villageCount:25,
 	power:395330
 }
