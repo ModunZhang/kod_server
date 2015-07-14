64e936be7afc9ead4b9420c654417457d1401dac
Modun Zhang <modunzhang@gmail.com>
2015-07-15 01:54:44 +0800

    debug

--- game-server/app/services/allianceApiService3.js
+++ game-server/app/services/allianceApiService3.js
@@ -518,7 +518,7 @@ pro.findAllianceToFight = function(playerId, allianceId, callback){
 			eventFuncs.push([self.timeEventService, self.timeEventService.removeAllianceTimeEventAsync, attackAllianceDoc, Consts.AllianceStatusEvent, Consts.AllianceStatusEvent])
 		}
 		var now = Date.now()
-		var finishTime = now + (DataUtils.getAllianceIntInit("allianceFightPrepareMinutes") * 60 * 1000)
+		var finishTime = now + (DataUtils.getAllianceIntInit("allianceFightPrepareMinutes") * 1 * 1000)
 		LogicUtils.prepareForAllianceFight(attackAllianceDoc, defenceAllianceDoc, finishTime)
 		attackAllianceData.push(["basicInfo", attackAllianceDoc.basicInfo])
 		attackAllianceData.push(["allianceFight", attackAllianceDoc.allianceFight])
