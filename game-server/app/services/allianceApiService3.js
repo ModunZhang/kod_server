58191f8bbd9c43335c0b47568132a67040bbd242
Modun Zhang <modunzhang@gmail.com>
2015-07-22 23:06:51 +0800

    debug

--- game-server/app/services/allianceApiService3.js
+++ game-server/app/services/allianceApiService3.js
@@ -508,6 +508,7 @@ pro.findAllianceToFight = function(playerId, allianceId, callback){
 		var powerSmall = _.isObject(docSmall) ? attackAllianceDoc.basicInfo.power - docSmall.basicInfo.power : null
 		var powerBig = _.isObject(docBig) ? docBig.basicInfo.power - attackAllianceDoc.basicInfo.power : null
 		var finalDoc = _.isNull(docSmall) ? docBig : _.isNull(docBig) ? docSmall : powerBig >= powerSmall ? docSmall : docBig
+		console.log(powerBig, powerSmall, finalDoc.basicInfo, '11111111111')
 		if(attackAllianceDoc.basicInfo.power * 1.9 < finalDoc.basicInfo.power || attackAllianceDoc.basicInfo.power * 0.1 > finalDoc.basicInfo.power)
 			return Promise.reject(ErrorUtils.canNotFindAllianceToFight(playerId, attackAllianceDoc._id))
 		return self.cacheService.findAllianceAsync(finalDoc._id)
