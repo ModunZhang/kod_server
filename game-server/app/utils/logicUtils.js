545f2da15b7c4305cba2652f9df312fe405d9874
Modun Zhang <modunzhang@gmail.com>
2015-08-12 15:56:08 +0800

    remove debug&配置玩家初始化数据

--- game-server/app/utils/logicUtils.js
+++ game-server/app/utils/logicUtils.js
@@ -2330,26 +2330,31 @@ Utils.initPlayerDoc = function(playerDoc){
 	}]
 	playerDoc.buildings.location_2.level = 1
 	playerDoc.buildings.location_1.level = 5
-	//playerDoc.soldiers.ranger = 100
-	//playerDoc.soldiers.swordsman = 100
-	//playerDoc.soldiers.skeletonWarrior = 1
-	//playerDoc.resources.citizen = 90
-	//playerDoc.items.push({
-	//	name:'changePlayerName',
-	//	count:1
-	//})
-	//playerDoc.items.push({
-	//	name:'moveTheCity',
-	//	count:1
-	//})
-	//playerDoc.items.push({
-	//	name:'ironBonus_1',
-	//	count:1
-	//})
-	//playerDoc.items.push({
-	//	name:'foodClass_2',
-	//	count:2
-	//})
+	playerDoc.soldiers.ranger = 100
+	playerDoc.soldiers.swordsman = 100
+	playerDoc.soldiers.skeletonWarrior = 1
+	playerDoc.resources.citizen = 90
+	playerDoc.items.push({
+		name:'changePlayerName',
+		count:1
+	})
+	playerDoc.items.push({
+		name:'moveTheCity',
+		count:1
+	})
+	playerDoc.items.push({
+		name:'foodClass_2',
+		count:1
+	})
+	playerDoc.items.push({
+		name:'woodClass_2',
+		count:1
+	})
+	playerDoc.pve.push({
+		"sections":[3, 3, 3],
+		"rewarded":[]
+	})
+	playerDoc.soldierMaterials.deathHand = 2;
 
 	DataUtils.refreshPlayerPower(playerDoc, [])
 }
