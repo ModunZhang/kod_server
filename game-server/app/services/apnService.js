90c733e65f326521c5a8cd5c55bc4f26c87ed4ef
Modun Zhang <modunzhang@gmail.com>
2015-08-12 20:33:42 +0800

    debug

--- game-server/app/services/apnService.js
+++ game-server/app/services/apnService.js
@@ -20,6 +20,7 @@ var DataUtils = require("../utils/dataUtils")
 var ApnService = function(app){
 	this.app = app
 	this.logService = app.get("logService")
+	console.log(app, '1111111111')
 	this.apnProductionMode = app.get('serverConfig').apnProductionMode;
 	this.apnPushCert = path.join(__dirname, '../../config/' + app.get('serverConfig').apnPushCert);
 	this.apnService = null
