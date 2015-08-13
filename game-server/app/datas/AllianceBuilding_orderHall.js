f283e42cba0d2c42d1028a0df33a5526ffce944c
Modun Zhang <modunzhang@gmail.com>
2015-08-13 22:04:52 +0800

    调整配置文件

--- game-server/app/datas/AllianceBuilding_orderHall.js
+++ game-server/app/datas/AllianceBuilding_orderHall.js
@@ -6,60 +6,60 @@ module.exports = orderHall
 orderHall[1] = {
 	level:1,
 	needHonour:5200,
-	villageCount:12,
+	villageCount:9,
 	power:775
 }
 orderHall[2] = {
 	level:2,
 	needHonour:10500,
-	villageCount:16,
+	villageCount:12,
 	power:1550
 }
 orderHall[3] = {
 	level:3,
 	needHonour:21000,
-	villageCount:20,
+	villageCount:15,
 	power:3105
 }
 orderHall[4] = {
 	level:4,
 	needHonour:42000,
-	villageCount:24,
+	villageCount:18,
 	power:6210
 }
 orderHall[5] = {
 	level:5,
 	needHonour:160300,
-	villageCount:28,
+	villageCount:21,
 	power:12425
 }
 orderHall[6] = {
 	level:6,
 	needHonour:562500,
-	villageCount:32,
+	villageCount:24,
 	power:24855
 }
 orderHall[7] = {
 	level:7,
 	needHonour:1701600,
-	villageCount:36,
+	villageCount:27,
 	power:57480
 }
 orderHall[8] = {
 	level:8,
 	needHonour:3448900,
-	villageCount:40,
+	villageCount:30,
 	power:108185
 }
 orderHall[9] = {
 	level:9,
 	needHonour:6491100,
-	villageCount:44,
+	villageCount:33,
 	power:214095
 }
 orderHall[10] = {
 	level:10,
 	needHonour:12845800,
-	villageCount:50,
+	villageCount:36,
 	power:395330
 }
