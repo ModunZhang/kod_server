274c41e51f3e2ea20f6390cbb12521c3ccc0d41f
Modun Zhang <modunzhang@gmail.com>
2015-08-20 21:44:25 +0800

    修复 GM Api 服务器Bug

--- game-server/app/servers/http/route/api.js
+++ game-server/app/servers/http/route/api.js
@@ -5,17 +5,21 @@
 
 
 module.exports = function(app, http){
-	var logService = app.get('logService');
-	var chatServerId = app.getServerFromConfig('chat-server-1').id;
+	http.all('*', function(req, res, next){
+		req.logService = app.get('logService');
+		req.chatServerId = app.getServerFromConfig('chat-server-1').id;
+		next();
+	});
+
 	http.post('/send-global-notice', function(req, res){
-		logService.onGm('/send-global-notice', req.body);
+		req.logService.onGm('/send-global-notice', req.body);
 
 		var servers = req.body.servers;
 		var type = req.body.type;
 		var content = req.body.content;
-		app.rpc.chat.chatRemote.sendGlobalNotice.toServer(chatServerId, servers, type, content, function(e){
+		app.rpc.chat.chatRemote.sendGlobalNotice.toServer(req.chatServerId, servers, type, content, function(e){
 			if(!!e){
-				logService.onGmError('/send-global-notice', req.body, e.stack);
+				req.logService.onGmError('/send-global-notice', req.body, e.stack);
 				res.json({code:500, data:e.message});
 			}else
 				res.json({code:200, data:null});
@@ -23,14 +27,14 @@ module.exports = function(app, http){
 	})
 
 	http.post('/send-global-mail', function(req, res){
-		logService.onGm('/send-global-mail', req.body);
+		req.logService.onGm('/send-global-mail', req.body);
 
 		var servers = req.body.servers;
 		var title = req.body.title;
 		var content = req.body.content;
-		app.rpc.chat.chatRemote.sendGlobalMail.toServer(chatServerId, servers, title, content, function(e){
+		app.rpc.chat.chatRemote.sendGlobalMail.toServer(req.chatServerId, servers, title, content, function(e){
 			if(!!e){
-				logService.onGmError('/send-global-mail', req.body, e.stack);
+				req.logService.onGmError('/send-global-mail', req.body, e.stack);
 				res.json({code:500, data:e.message});
 			}else
 				res.json({code:200, data:null});
@@ -38,7 +42,7 @@ module.exports = function(app, http){
 	})
 
 	http.post('/send-mail-to-players', function(req, res){
-		logService.onGm('/send-mail-to-players', req.body);
+		req.logService.onGm('/send-mail-to-players', req.body);
 
 		var serverId = req.body.serverId;
 		var ids = req.body.ids;
@@ -46,7 +50,7 @@ module.exports = function(app, http){
 		var content = req.body.content;
 		app.rpc.cache.cacheRemote.sendMailToPlayers.toServer(serverId, ids, title, content, function(e){
 			if(!!e){
-				logService.onGmError('/send-mail-to-players', req.body, e.stack);
+				req.logService.onGmError('/send-mail-to-players', req.body, e.stack);
 				res.json({code:500, data:e.message});
 			}else
 				res.json({code:200, data:null});
