76a6939f3a257dd097f917795b4b1f39733d43e0
Modun Zhang <modunzhang@gmail.com>
2015-08-21 14:48:03 +0800

    玩家GM Api

--- game-server/app/servers/http/route/api.js
+++ game-server/app/servers/http/route/api.js
@@ -44,11 +44,11 @@ module.exports = function(app, http){
 	http.post('/send-mail-to-players', function(req, res){
 		req.logService.onGm('/send-mail-to-players', req.body);
 
-		var serverId = req.body.serverId;
-		var ids = req.body.ids;
+		var server = req.body.server;
+		var players = req.body.players;
 		var title = req.body.title;
 		var content = req.body.content;
-		app.rpc.cache.cacheRemote.sendMailToPlayers.toServer(serverId, ids, title, content, function(e){
+		app.rpc.cache.cacheRemote.sendMailToPlayers.toServer(server, players, title, content, function(e){
 			if(!!e){
 				req.logService.onGmError('/send-mail-to-players', req.body, e.stack);
 				res.json({code:500, data:e.message});
