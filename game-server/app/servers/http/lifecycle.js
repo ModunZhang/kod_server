1162975061ecf39ae8e5a0edd2b0b8402eac4727
Modun Zhang <modunzhang@gmail.com>
2015-09-07 09:32:44 +0800

    mongo 数据库备份

--- game-server/app/servers/http/lifecycle.js
+++ game-server/app/servers/http/lifecycle.js
@@ -5,6 +5,7 @@
  */
 
 var Promise = require("bluebird")
+var mongoBackup = require('mongodb_s3_backup')
 
 var LogService = require("../../services/logService")
 
@@ -27,4 +28,18 @@ life.beforeShutdown = function(app, callback){
 
 life.afterStartAll = function(app){
 	app.get("logService").onEvent("server started", {serverId:app.getServerId()})
+	if(app.get("env") === 'local') return;
+
+	var config = app.get('serverConfig').mongoBackup;
+	(function backupMongo(){
+		var timeout = setTimeout(function(){
+			app.get("logService").onEvent('mongo backup start');
+			mongoBackup.sync(config.mongodb, config.s3, function(e){
+				if(!!e) app.get("logService").onEventError('mongo backup finished with error', null, e.stack);
+				else app.get("logService").onEvent('mongo backup finished');
+				backupMongo();
+			})
+		}, 1000 * 10);//1000 * 60 * 60 * 4);
+		timeout.unref();
+	})();
 }
