09ced0c6305867a540048e846f3e0bc0b45cc1f6
Modun Zhang <modunzhang@gmail.com>
2015-10-21 09:20:24 +0800

    调整配置文件

---
+++ game-server/app/datas/AllianceMap_moveLimit.js
@@ -0,0 +1,89 @@
+"use strict"
+
+var moveLimit = []
+module.exports = moveLimit
+
+moveLimit[0] = {
+	round:0,
+	needPalaceLevel:50
+}
+moveLimit[1] = {
+	round:1,
+	needPalaceLevel:35
+}
+moveLimit[2] = {
+	round:2,
+	needPalaceLevel:30
+}
+moveLimit[3] = {
+	round:3,
+	needPalaceLevel:28
+}
+moveLimit[4] = {
+	round:4,
+	needPalaceLevel:26
+}
+moveLimit[5] = {
+	round:5,
+	needPalaceLevel:24
+}
+moveLimit[6] = {
+	round:6,
+	needPalaceLevel:22
+}
+moveLimit[7] = {
+	round:7,
+	needPalaceLevel:20
+}
+moveLimit[8] = {
+	round:8,
+	needPalaceLevel:18
+}
+moveLimit[9] = {
+	round:9,
+	needPalaceLevel:16
+}
+moveLimit[10] = {
+	round:10,
+	needPalaceLevel:14
+}
+moveLimit[11] = {
+	round:11,
+	needPalaceLevel:12
+}
+moveLimit[12] = {
+	round:12,
+	needPalaceLevel:10
+}
+moveLimit[13] = {
+	round:13,
+	needPalaceLevel:8
+}
+moveLimit[14] = {
+	round:14,
+	needPalaceLevel:6
+}
+moveLimit[15] = {
+	round:15,
+	needPalaceLevel:4
+}
+moveLimit[16] = {
+	round:16,
+	needPalaceLevel:2
+}
+moveLimit[17] = {
+	round:17,
+	needPalaceLevel:2
+}
+moveLimit[18] = {
+	round:18,
+	needPalaceLevel:1
+}
+moveLimit[19] = {
+	round:19,
+	needPalaceLevel:1
+}
+moveLimit[20] = {
+	round:20,
+	needPalaceLevel:0
+}
