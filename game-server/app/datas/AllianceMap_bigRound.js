43b29fc18032d0d98360a5586517e17db77cf19b
Modun Zhang <modunzhang@gmail.com>
2015-10-28 10:10:13 +0800

    调整配置问价

---
+++ game-server/app/datas/AllianceMap_bigRound.js
@@ -0,0 +1,89 @@
+"use strict"
+
+var bigRound = []
+module.exports = bigRound
+
+bigRound[1] = {
+	index:1,
+	locationFromX:0,
+	locationFromY:0,
+	locationToX:9,
+	locationToY:9
+}
+bigRound[2] = {
+	index:2,
+	locationFromX:10,
+	locationFromY:0,
+	locationToX:19,
+	locationToY:9
+}
+bigRound[3] = {
+	index:3,
+	locationFromX:0,
+	locationFromY:10,
+	locationToX:9,
+	locationToY:19
+}
+bigRound[4] = {
+	index:4,
+	locationFromX:21,
+	locationFromY:0,
+	locationToX:30,
+	locationToY:9
+}
+bigRound[5] = {
+	index:5,
+	locationFromX:31,
+	locationFromY:0,
+	locationToX:40,
+	locationToY:9
+}
+bigRound[6] = {
+	index:6,
+	locationFromX:31,
+	locationFromY:10,
+	locationToX:40,
+	locationToY:19
+}
+bigRound[7] = {
+	index:7,
+	locationFromX:0,
+	locationFromY:21,
+	locationToX:9,
+	locationToY:30
+}
+bigRound[8] = {
+	index:8,
+	locationFromX:0,
+	locationFromY:31,
+	locationToX:9,
+	locationToY:40
+}
+bigRound[9] = {
+	index:9,
+	locationFromX:10,
+	locationFromY:31,
+	locationToX:19,
+	locationToY:40
+}
+bigRound[10] = {
+	index:10,
+	locationFromX:31,
+	locationFromY:21,
+	locationToX:40,
+	locationToY:30
+}
+bigRound[11] = {
+	index:11,
+	locationFromX:31,
+	locationFromY:31,
+	locationToX:40,
+	locationToY:40
+}
+bigRound[12] = {
+	index:12,
+	locationFromX:21,
+	locationFromY:31,
+	locationToX:30,
+	locationToY:40
+}
