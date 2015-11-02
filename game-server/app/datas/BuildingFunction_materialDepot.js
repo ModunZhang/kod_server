a7378af2435072772529fed671e3040d07d386af
Modun Zhang <modunzhang@gmail.com>
2015-11-02 20:57:21 +0800

    调整配置文件

--- game-server/app/datas/BuildingFunction_materialDepot.js
+++ game-server/app/datas/BuildingFunction_materialDepot.js
@@ -5,321 +5,321 @@ module.exports = materialDepot
 
 materialDepot[1] = {
 	level:1,
-	soldierMaterials:20,
-	buildingMaterials:20,
-	technologyMaterials:20,
-	dragonMaterials:20,
-	power:15
-}
-materialDepot[2] = {
-	level:2,
 	soldierMaterials:40,
 	buildingMaterials:40,
 	technologyMaterials:40,
 	dragonMaterials:40,
-	power:20
+	power:15
 }
-materialDepot[3] = {
-	level:3,
+materialDepot[2] = {
+	level:2,
 	soldierMaterials:80,
 	buildingMaterials:80,
 	technologyMaterials:80,
 	dragonMaterials:80,
-	power:25
+	power:20
 }
-materialDepot[4] = {
-	level:4,
-	soldierMaterials:120,
-	buildingMaterials:120,
-	technologyMaterials:120,
-	dragonMaterials:120,
-	power:30
-}
-materialDepot[5] = {
-	level:5,
+materialDepot[3] = {
+	level:3,
 	soldierMaterials:160,
 	buildingMaterials:160,
 	technologyMaterials:160,
 	dragonMaterials:160,
-	power:40
+	power:25
 }
-materialDepot[6] = {
-	level:6,
-	soldierMaterials:200,
-	buildingMaterials:200,
-	technologyMaterials:200,
-	dragonMaterials:200,
-	power:50
-}
-materialDepot[7] = {
-	level:7,
+materialDepot[4] = {
+	level:4,
 	soldierMaterials:240,
 	buildingMaterials:240,
 	technologyMaterials:240,
 	dragonMaterials:240,
-	power:80
+	power:30
 }
-materialDepot[8] = {
-	level:8,
-	soldierMaterials:280,
-	buildingMaterials:280,
-	technologyMaterials:280,
-	dragonMaterials:280,
-	power:160
-}
-materialDepot[9] = {
-	level:9,
+materialDepot[5] = {
+	level:5,
 	soldierMaterials:320,
 	buildingMaterials:320,
 	technologyMaterials:320,
 	dragonMaterials:320,
-	power:350
+	power:40
 }
-materialDepot[10] = {
-	level:10,
-	soldierMaterials:360,
-	buildingMaterials:360,
-	technologyMaterials:360,
-	dragonMaterials:360,
-	power:520
-}
-materialDepot[11] = {
-	level:11,
+materialDepot[6] = {
+	level:6,
 	soldierMaterials:400,
 	buildingMaterials:400,
 	technologyMaterials:400,
 	dragonMaterials:400,
-	power:730
+	power:50
 }
-materialDepot[12] = {
-	level:12,
-	soldierMaterials:440,
-	buildingMaterials:440,
-	technologyMaterials:440,
-	dragonMaterials:440,
-	power:920
-}
-materialDepot[13] = {
-	level:13,
+materialDepot[7] = {
+	level:7,
 	soldierMaterials:480,
 	buildingMaterials:480,
 	technologyMaterials:480,
 	dragonMaterials:480,
-	power:1220
+	power:80
 }
-materialDepot[14] = {
-	level:14,
-	soldierMaterials:520,
-	buildingMaterials:520,
-	technologyMaterials:520,
-	dragonMaterials:520,
-	power:2470
-}
-materialDepot[15] = {
-	level:15,
+materialDepot[8] = {
+	level:8,
 	soldierMaterials:560,
 	buildingMaterials:560,
 	technologyMaterials:560,
 	dragonMaterials:560,
-	power:3860
+	power:160
 }
-materialDepot[16] = {
-	level:16,
-	soldierMaterials:600,
-	buildingMaterials:600,
-	technologyMaterials:600,
-	dragonMaterials:600,
-	power:5340
-}
-materialDepot[17] = {
-	level:17,
+materialDepot[9] = {
+	level:9,
 	soldierMaterials:640,
 	buildingMaterials:640,
 	technologyMaterials:640,
 	dragonMaterials:640,
-	power:7800
+	power:350
 }
-materialDepot[18] = {
-	level:18,
-	soldierMaterials:680,
-	buildingMaterials:680,
-	technologyMaterials:680,
-	dragonMaterials:680,
-	power:9610
-}
-materialDepot[19] = {
-	level:19,
+materialDepot[10] = {
+	level:10,
 	soldierMaterials:720,
 	buildingMaterials:720,
 	technologyMaterials:720,
 	dragonMaterials:720,
-	power:11730
+	power:520
 }
-materialDepot[20] = {
-	level:20,
-	soldierMaterials:760,
-	buildingMaterials:760,
-	technologyMaterials:760,
-	dragonMaterials:760,
-	power:13770
-}
-materialDepot[21] = {
-	level:21,
+materialDepot[11] = {
+	level:11,
 	soldierMaterials:800,
 	buildingMaterials:800,
 	technologyMaterials:800,
 	dragonMaterials:800,
-	power:18700
+	power:730
 }
-materialDepot[22] = {
-	level:22,
-	soldierMaterials:840,
-	buildingMaterials:840,
-	technologyMaterials:840,
-	dragonMaterials:840,
-	power:21440
-}
-materialDepot[23] = {
-	level:23,
+materialDepot[12] = {
+	level:12,
 	soldierMaterials:880,
 	buildingMaterials:880,
 	technologyMaterials:880,
 	dragonMaterials:880,
-	power:24740
+	power:920
 }
-materialDepot[24] = {
-	level:24,
-	soldierMaterials:920,
-	buildingMaterials:920,
-	technologyMaterials:920,
-	dragonMaterials:920,
-	power:27850
-}
-materialDepot[25] = {
-	level:25,
+materialDepot[13] = {
+	level:13,
 	soldierMaterials:960,
 	buildingMaterials:960,
 	technologyMaterials:960,
 	dragonMaterials:960,
-	power:38030
+	power:1220
 }
-materialDepot[26] = {
-	level:26,
-	soldierMaterials:1000,
-	buildingMaterials:1000,
-	technologyMaterials:1000,
-	dragonMaterials:1000,
-	power:42460
+materialDepot[14] = {
+	level:14,
+	soldierMaterials:1040,
+	buildingMaterials:1040,
+	technologyMaterials:1040,
+	dragonMaterials:1040,
+	power:2470
 }
-materialDepot[27] = {
-	level:27,
-	soldierMaterials:1100,
-	buildingMaterials:1100,
-	technologyMaterials:1100,
-	dragonMaterials:1100,
-	power:47770
+materialDepot[15] = {
+	level:15,
+	soldierMaterials:1120,
+	buildingMaterials:1120,
+	technologyMaterials:1120,
+	dragonMaterials:1120,
+	power:3860
 }
-materialDepot[28] = {
-	level:28,
+materialDepot[16] = {
+	level:16,
 	soldierMaterials:1200,
 	buildingMaterials:1200,
 	technologyMaterials:1200,
 	dragonMaterials:1200,
-	power:52780
+	power:5340
 }
-materialDepot[29] = {
-	level:29,
-	soldierMaterials:1400,
-	buildingMaterials:1400,
-	technologyMaterials:1400,
-	dragonMaterials:1400,
-	power:75130
+materialDepot[17] = {
+	level:17,
+	soldierMaterials:1280,
+	buildingMaterials:1280,
+	technologyMaterials:1280,
+	dragonMaterials:1280,
+	power:7800
 }
-materialDepot[30] = {
-	level:30,
+materialDepot[18] = {
+	level:18,
+	soldierMaterials:1360,
+	buildingMaterials:1360,
+	technologyMaterials:1360,
+	dragonMaterials:1360,
+	power:9610
+}
+materialDepot[19] = {
+	level:19,
+	soldierMaterials:1440,
+	buildingMaterials:1440,
+	technologyMaterials:1440,
+	dragonMaterials:1440,
+	power:11730
+}
+materialDepot[20] = {
+	level:20,
+	soldierMaterials:1520,
+	buildingMaterials:1520,
+	technologyMaterials:1520,
+	dragonMaterials:1520,
+	power:13770
+}
+materialDepot[21] = {
+	level:21,
 	soldierMaterials:1600,
 	buildingMaterials:1600,
 	technologyMaterials:1600,
 	dragonMaterials:1600,
-	power:86730
+	power:18700
 }
-materialDepot[31] = {
-	level:31,
-	soldierMaterials:1800,
-	buildingMaterials:1800,
-	technologyMaterials:1800,
-	dragonMaterials:1800,
-	power:100170
+materialDepot[22] = {
+	level:22,
+	soldierMaterials:1680,
+	buildingMaterials:1680,
+	technologyMaterials:1680,
+	dragonMaterials:1680,
+	power:21440
 }
-materialDepot[32] = {
-	level:32,
+materialDepot[23] = {
+	level:23,
+	soldierMaterials:1760,
+	buildingMaterials:1760,
+	technologyMaterials:1760,
+	dragonMaterials:1760,
+	power:24740
+}
+materialDepot[24] = {
+	level:24,
+	soldierMaterials:1840,
+	buildingMaterials:1840,
+	technologyMaterials:1840,
+	dragonMaterials:1840,
+	power:27850
+}
+materialDepot[25] = {
+	level:25,
+	soldierMaterials:1920,
+	buildingMaterials:1920,
+	technologyMaterials:1920,
+	dragonMaterials:1920,
+	power:38030
+}
+materialDepot[26] = {
+	level:26,
 	soldierMaterials:2000,
 	buildingMaterials:2000,
 	technologyMaterials:2000,
 	dragonMaterials:2000,
-	power:113650
+	power:42460
 }
-materialDepot[33] = {
-	level:33,
+materialDepot[27] = {
+	level:27,
+	soldierMaterials:2200,
+	buildingMaterials:2200,
+	technologyMaterials:2200,
+	dragonMaterials:2200,
+	power:47770
+}
+materialDepot[28] = {
+	level:28,
 	soldierMaterials:2400,
 	buildingMaterials:2400,
 	technologyMaterials:2400,
 	dragonMaterials:2400,
-	power:155560
+	power:52780
 }
-materialDepot[34] = {
-	level:34,
+materialDepot[29] = {
+	level:29,
 	soldierMaterials:2800,
 	buildingMaterials:2800,
 	technologyMaterials:2800,
 	dragonMaterials:2800,
-	power:174800
+	power:75130
 }
-materialDepot[35] = {
-	level:35,
+materialDepot[30] = {
+	level:30,
 	soldierMaterials:3200,
 	buildingMaterials:3200,
 	technologyMaterials:3200,
 	dragonMaterials:3200,
-	power:196900
+	power:86730
 }
-materialDepot[36] = {
-	level:36,
+materialDepot[31] = {
+	level:31,
 	soldierMaterials:3600,
 	buildingMaterials:3600,
 	technologyMaterials:3600,
 	dragonMaterials:3600,
-	power:219150
+	power:100170
 }
-materialDepot[37] = {
-	level:37,
+materialDepot[32] = {
+	level:32,
 	soldierMaterials:4000,
 	buildingMaterials:4000,
 	technologyMaterials:4000,
 	dragonMaterials:4000,
+	power:113650
+}
+materialDepot[33] = {
+	level:33,
+	soldierMaterials:4800,
+	buildingMaterials:4800,
+	technologyMaterials:4800,
+	dragonMaterials:4800,
+	power:155560
+}
+materialDepot[34] = {
+	level:34,
+	soldierMaterials:5600,
+	buildingMaterials:5600,
+	technologyMaterials:5600,
+	dragonMaterials:5600,
+	power:174800
+}
+materialDepot[35] = {
+	level:35,
+	soldierMaterials:6400,
+	buildingMaterials:6400,
+	technologyMaterials:6400,
+	dragonMaterials:6400,
+	power:196900
+}
+materialDepot[36] = {
+	level:36,
+	soldierMaterials:7200,
+	buildingMaterials:7200,
+	technologyMaterials:7200,
+	dragonMaterials:7200,
+	power:219150
+}
+materialDepot[37] = {
+	level:37,
+	soldierMaterials:8000,
+	buildingMaterials:8000,
+	technologyMaterials:8000,
+	dragonMaterials:8000,
 	power:294720
 }
 materialDepot[38] = {
 	level:38,
-	soldierMaterials:4400,
-	buildingMaterials:4400,
-	technologyMaterials:4400,
-	dragonMaterials:4400,
+	soldierMaterials:8800,
+	buildingMaterials:8800,
+	technologyMaterials:8800,
+	dragonMaterials:8800,
 	power:326020
 }
 materialDepot[39] = {
 	level:39,
-	soldierMaterials:5000,
-	buildingMaterials:5000,
-	technologyMaterials:5000,
-	dragonMaterials:5000,
+	soldierMaterials:10000,
+	buildingMaterials:10000,
+	technologyMaterials:10000,
+	dragonMaterials:10000,
 	power:361440
 }
 materialDepot[40] = {
 	level:40,
-	soldierMaterials:6000,
-	buildingMaterials:6000,
-	technologyMaterials:6000,
-	dragonMaterials:6000,
+	soldierMaterials:12000,
+	buildingMaterials:12000,
+	technologyMaterials:12000,
+	dragonMaterials:12000,
 	power:397570
 }
