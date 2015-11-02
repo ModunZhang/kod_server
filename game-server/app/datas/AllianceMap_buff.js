c333063e2cca2b4cf53b656603a3c2fe40c32e33
Modun Zhang <modunzhang@gmail.com>
2015-11-02 17:59:10 +0800

    调整配置文件

--- game-server/app/datas/AllianceMap_buff.js
+++ game-server/app/datas/AllianceMap_buff.js
@@ -9,186 +9,186 @@ buff[0] = {
 	loyaltyAddPercent:50,
 	honourAddPercent:50,
 	dragonExpAddPercent:24,
-	bloodAddPercent:24,
+	bloodAddPercent:25,
 	marchSpeedAddPercent:20,
-	dragonStrengthAddPercent:15,
+	dragonStrengthAddPercent:10,
 	monsterLevel:"36_40"
 }
 buff[1] = {
 	round:1,
-	villageAddPercent:35,
-	loyaltyAddPercent:35,
-	honourAddPercent:35,
-	dragonExpAddPercent:16,
-	bloodAddPercent:16,
-	marchSpeedAddPercent:12,
-	dragonStrengthAddPercent:10,
-	monsterLevel:"33_35"
-}
-buff[2] = {
-	round:2,
 	villageAddPercent:30,
 	loyaltyAddPercent:30,
 	honourAddPercent:30,
-	dragonExpAddPercent:12,
-	bloodAddPercent:12,
-	marchSpeedAddPercent:9,
-	dragonStrengthAddPercent:6,
-	monsterLevel:"30_32"
+	dragonExpAddPercent:16,
+	bloodAddPercent:15,
+	marchSpeedAddPercent:10,
+	dragonStrengthAddPercent:5,
+	monsterLevel:"31_35"
 }
-buff[3] = {
-	round:3,
-	villageAddPercent:25,
-	loyaltyAddPercent:25,
-	honourAddPercent:25,
-	dragonExpAddPercent:10,
-	bloodAddPercent:9,
-	marchSpeedAddPercent:6,
-	dragonStrengthAddPercent:3,
-	monsterLevel:"27_29"
-}
-buff[4] = {
-	round:4,
+buff[2] = {
+	round:2,
 	villageAddPercent:20,
 	loyaltyAddPercent:20,
 	honourAddPercent:20,
-	dragonExpAddPercent:8,
-	bloodAddPercent:6,
-	marchSpeedAddPercent:3,
+	dragonExpAddPercent:12,
+	bloodAddPercent:10,
+	marchSpeedAddPercent:5,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"24_26"
+	monsterLevel:"26_30"
 }
-buff[5] = {
-	round:5,
+buff[3] = {
+	round:3,
 	villageAddPercent:15,
 	loyaltyAddPercent:15,
 	honourAddPercent:15,
-	dragonExpAddPercent:6,
-	bloodAddPercent:3,
+	dragonExpAddPercent:10,
+	bloodAddPercent:5,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"21_23"
+	monsterLevel:"21_25"
 }
-buff[6] = {
-	round:6,
-	villageAddPercent:12,
+buff[4] = {
+	round:4,
+	villageAddPercent:10,
 	loyaltyAddPercent:10,
 	honourAddPercent:10,
-	dragonExpAddPercent:4,
+	dragonExpAddPercent:5,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"18_20"
+	monsterLevel:"16_20"
 }
-buff[7] = {
-	round:7,
-	villageAddPercent:9,
+buff[5] = {
+	round:5,
+	villageAddPercent:5,
 	loyaltyAddPercent:5,
 	honourAddPercent:5,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"15_17"
+	monsterLevel:"11_15"
+}
+buff[6] = {
+	round:6,
+	villageAddPercent:2,
+	loyaltyAddPercent:0,
+	honourAddPercent:0,
+	dragonExpAddPercent:0,
+	bloodAddPercent:0,
+	marchSpeedAddPercent:0,
+	dragonStrengthAddPercent:0,
+	monsterLevel:"5_10"
+}
+buff[7] = {
+	round:7,
+	villageAddPercent:2,
+	loyaltyAddPercent:0,
+	honourAddPercent:0,
+	dragonExpAddPercent:0,
+	bloodAddPercent:0,
+	marchSpeedAddPercent:0,
+	dragonStrengthAddPercent:0,
+	monsterLevel:"5_10"
 }
 buff[8] = {
 	round:8,
-	villageAddPercent:6,
-	loyaltyAddPercent:2,
+	villageAddPercent:2,
+	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"12_14"
+	monsterLevel:"5_10"
 }
 buff[9] = {
 	round:9,
-	villageAddPercent:3,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"9_11"
+	monsterLevel:"5_10"
 }
 buff[10] = {
 	round:10,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[11] = {
 	round:11,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[12] = {
 	round:12,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[13] = {
 	round:13,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[14] = {
 	round:14,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[15] = {
 	round:15,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[16] = {
 	round:16,
-	villageAddPercent:0,
+	villageAddPercent:2,
 	loyaltyAddPercent:0,
 	honourAddPercent:0,
 	dragonExpAddPercent:0,
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"5_10"
 }
 buff[17] = {
 	round:17,
@@ -199,5 +199,5 @@ buff[17] = {
 	bloodAddPercent:0,
 	marchSpeedAddPercent:0,
 	dragonStrengthAddPercent:0,
-	monsterLevel:"1_8"
+	monsterLevel:"1_5"
 }
--- game-server/app/datas/AllianceMap_moveLimit.js
+++ game-server/app/datas/AllianceMap_moveLimit.js
@@ -9,39 +9,39 @@ moveLimit[0] = {
 }
 moveLimit[1] = {
 	round:1,
-	needPalaceLevel:13
+	needPalaceLevel:12
 }
 moveLimit[2] = {
 	round:2,
-	needPalaceLevel:11
+	needPalaceLevel:9
 }
 moveLimit[3] = {
 	round:3,
-	needPalaceLevel:9
+	needPalaceLevel:7
 }
 moveLimit[4] = {
 	round:4,
-	needPalaceLevel:7
+	needPalaceLevel:5
 }
 moveLimit[5] = {
 	round:5,
-	needPalaceLevel:6
+	needPalaceLevel:3
 }
 moveLimit[6] = {
 	round:6,
-	needPalaceLevel:5
+	needPalaceLevel:1
 }
 moveLimit[7] = {
 	round:7,
-	needPalaceLevel:4
+	needPalaceLevel:1
 }
 moveLimit[8] = {
 	round:8,
-	needPalaceLevel:3
+	needPalaceLevel:1
 }
 moveLimit[9] = {
 	round:9,
-	needPalaceLevel:2
+	needPalaceLevel:1
 }
 moveLimit[10] = {
 	round:10,
