a2b3bbd03edd94f6ce3d9b03c8c69f0d8a73734e
Modun Zhang <modunzhang@gmail.com>
2015-12-03 21:04:58 +0800

    修改配置文件

--- game-server/app/datas/Localizations_alliance.js
+++ game-server/app/datas/Localizations_alliance.js
@@ -6,234 +6,234 @@ module.exports = alliance
 alliance["RequestRejectedTitle"] = {
 	key:"RequestRejectedTitle",
 	cn:"联盟申请被拒绝",
-	en:"这是英文:联盟申请被拒绝",
+	en:"Your application was rejected",
 	tw:"聯盟申請被拒絕"
 }
 alliance["RequestRejectedContent"] = {
 	key:"RequestRejectedContent",
 	cn:"主人,你对 %s 联盟的申请被无情的拒绝了。",
-	en:"这是英文:主人,你对 %s 联盟的申请被无情的拒绝了。",
+	en:"My lord, %s Alliance has rejected your application.",
 	tw:"主人,你對 %s 聯盟的申請被無情的拒絕了。"
 }
 alliance["RequestApprovedTitle"] = {
 	key:"RequestApprovedTitle",
 	cn:"联盟申请审核通过",
-	en:"这是英文:联盟申请审核通过",
+	en:"Your application was approved",
 	tw:"聯盟申請審核通過"
 }
 alliance["RequestApprovedContent"] = {
 	key:"RequestApprovedContent",
 	cn:"恭喜,你对 %s 联盟的申请审核通过了。",
-	en:"这是英文:恭喜,你对 %s 联盟的申请审核通过了。",
+	en:"Congratulations! %s Alliance has approved your application.",
 	tw:"恭喜,你對 %s 聯盟的申請審核通過了。"
 }
 alliance["InviteRejectedTitle"] = {
 	key:"InviteRejectedTitle",
 	cn:"加入联盟邀请被拒绝",
-	en:"这是英文:加入联盟邀请被拒绝",
+	en:"Your invitation was rejected",
 	tw:"加入聯盟邀請被拒絕"
 }
 alliance["InviteRejectedContent"] = {
 	key:"InviteRejectedContent",
 	cn:"主人,%s 拒绝了你对他加入联盟的邀请。",
-	en:"这是英文:主人,%s 拒绝了你对他加入联盟的邀请。",
+	en:"My lord, %s has rejected your invitation.",
 	tw:"主人,%s 拒絕了你對他加入聯盟的邀請。"
 }
 alliance["InviteApprovedTitle"] = {
 	key:"InviteApprovedTitle",
 	cn:"加入联盟邀请被接受",
-	en:"这是英文:加入联盟邀请被接受",
+	en:"Your invitation was accepted",
 	tw:"加入聯盟邀請被接受"
 }
 alliance["InviteApprovedContent"] = {
 	key:"InviteApprovedContent",
 	cn:"恭喜,%s 接受了你对他加入联盟的邀请。",
-	en:"这是英文:恭喜,%s 接受了你对他加入联盟的邀请。",
+	en:"Congratulations! %s has accepted your invitation.",
 	tw:"恭喜,%s 接受了你對他加入聯盟的邀請。"
 }
 alliance["HelpDefenceStrikeTitle"] = {
 	key:"HelpDefenceStrikeTitle",
-	cn:"[%s]%s帮你抵御了一次突袭",
-	en:"这是英文[%s]%s帮你抵御了一次突袭",
+	cn:"[%s]%s帮你抵御了一次侦察",
+	en:"[%s]%s helped you to defend a enemy scout",
 	tw:"[%s]%s幫你抵禦了一次突襲"
 }
 alliance["HelpDefenceStrikeContent"] = {
 	key:"HelpDefenceStrikeContent",
-	cn:"[%s]%s帮你抵御了一次突袭。",
-	en:"这是英文:[%s]%s帮你抵御了一次突袭。",
+	cn:"[%s]%s帮你抵御了一次侦察。",
+	en:"[%s]%s helped you to defend a enemy scout.",
 	tw:"[%s]%s幫你抵禦了一次突襲。"
 }
 alliance["HelpDefenceAttackTitle"] = {
 	key:"HelpDefenceAttackTitle",
 	cn:"[%s]%s帮你抵御了一次进攻",
-	en:"这是英文:[%s]%s帮你抵御了一次进攻",
+	en:"[%s]%s helped you to defend a enemy attack",
 	tw:"[%s]%s幫你抵禦了一次進攻"
 }
 alliance["HelpDefenceAttackContent"] = {
 	key:"HelpDefenceAttackContent",
 	cn:"[%s]%s帮你抵御了一次进攻。",
-	en:"这是英文:[%s]%s帮你抵御了一次进攻。",
+	en:"[%s]%s helped you to defend a enemy attack.",
 	tw:"[%s]%s幫你抵禦了一次進攻。"
 }
 alliance["AttackAlliancePrepare"] = {
 	key:"AttackAlliancePrepare",
 	cn:"我方联盟发起了一场联盟会战，战斗将于15分钟后开始，请做好战斗准备！",
-	en:"这是英文:我方联盟刚刚发起了一场联盟会战，战斗将于15分钟后开始，请做好战斗准备！",
+	en:"Your Alliance has declared a war, and it will start in 15 minutes, prepare for the battle!",
 	tw:"我方聯盟剛剛發起了一場聯盟會戰，戰鬥將於15分鐘後開始，請做好戰鬥準備！"
 }
 alliance["AllianceBeAttackedPrepare"] = {
 	key:"AllianceBeAttackedPrepare",
 	cn:"%s联盟向我方发起了一场联盟会战，战斗将于15分钟后开始，请做好战斗准备！",
-	en:"这是英文:%s联盟对我方发起了一场联盟会战，战斗将于15分钟后开始，请做好战斗准备！",
+	en:"%s has declared a war to your Alliance, and it will start in 15 minutes, prepare for the battle!",
 	tw:"%s聯盟向我方發起了一場聯盟會戰，戰鬥將於15分鐘後開始，請做好戰鬥準備！"
 }
 alliance["AttackAllianceStart"] = {
 	key:"AttackAllianceStart",
 	cn:"与%s联盟的战斗已经开始！",
-	en:"这是英文:与%s联盟的战斗已经开始！",
+	en:"The battle with %s has already begun!",
 	tw:"與%s聯盟的戰鬥已經開始！"
 }
 alliance["AllianceBeAttackedStart"] = {
 	key:"AllianceBeAttackedStart",
 	cn:"与%s联盟的战斗已经开始！",
-	en:"这是英文:与%s联盟的战斗已经开始！",
+	en:"The battle with %s has already begun!",
 	tw:"與%s聯盟的戰鬥已經開始！"
 }
 alliance["AllianceTitleBeModifyedTitle"] = {
 	key:"AllianceTitleBeModifyedTitle",
 	cn:"你的联盟职位被调整",
-	en:"这是英文:你的联盟职位被调整",
+	en:"Your Alliance position has been adjusted",
 	tw:"你的聯盟職位被調整"
 }
 alliance["AllianceTitleBeModifyedContent"] = {
 	key:"AllianceTitleBeModifyedContent",
 	cn:"你的职位由%s调整为%s。",
-	en:"这是英文:你的职位由%s调整为%s。",
+	en:"Your Alliance position has been adjusted to %s.",
 	tw:"你的職位由%s調整為%s。"
 }
 alliance["AllianceKickMemberOffTitle"] = {
 	key:"AllianceKickMemberOffTitle",
 	cn:"你已被%s联盟踢出",
-	en:"这是英文:你已被%s联盟踢出",
+	en:"You have been kicked out",
 	tw:"你已被%s聯盟踢出"
 }
 alliance["AllianceKickMemberOffContent"] = {
 	key:"AllianceKickMemberOffContent",
 	cn:"你已被%s联盟踢出。",
-	en:"这是英文:你已被%s联盟踢出。",
+	en:"You have been kicked out by %s Alliance.",
 	tw:"你已被%s聯盟踢出。"
 }
 alliance["giveLoyaltyToAllianceMemberTitle"] = {
 	key:"giveLoyaltyToAllianceMemberTitle",
 	cn:"忠诚值奖励",
-	en:"这是英文:忠诚值奖励",
+	en:"Loyalty reward",
 	tw:"忠誠值獎勵"
 }
 alliance["giveLoyaltyToAllianceMemberContent"] = {
 	key:"giveLoyaltyToAllianceMemberContent",
 	cn:"%s联盟向你发放了%d点忠诚值奖励，请再接再厉！",
-	en:"这是英文:%s联盟向你发放了%d点忠诚值奖励，请再接再厉！",
+	en:"%s Alliance granted you %s loyalty points for your heroic!",
 	tw:"%s聯盟向你發放了%d點忠誠值獎勵，請再接再厲！"
 }
 alliance["AllianceFightKillFirstRewardTitle"] = {
 	key:"AllianceFightKillFirstRewardTitle",
 	cn:"联盟战最高击杀奖励",
-	en:"这是英文:联盟战最高击杀奖励",
+	en:"NO.1 Killer prize",
 	tw:"聯盟戰最高擊殺獎勵"
 }
 alliance["AllianceFightKillFirstRewardContent"] = {
 	key:"AllianceFightKillFirstRewardContent",
 	cn:"你在联盟会战中奋勇杀敌，成为“头号杀手”,系统已奖励%d金龙币到你的账户中。",
-	en:"这是英文:你在联盟会战中奋勇杀敌，成为“头号杀手”,系统已奖励%d金龙币到你的账户中。",
+	en:"You received %s Dragon Coins as the NO.1 Killer prize in the last Alliance Battle.",
 	tw:"你在聯盟會戰中奮勇殺敵，成為“頭號殺手”,系統已獎勵%d金龍幣到你的賬戶中。"
 }
 alliance["AllianceShrineEventStart"] = {
 	key:"AllianceShrineEventStart",
 	cn:"我方联盟激活了一场圣地战,并将于20分钟后结束,请做好出兵准备！",
-	en:"这是英文:我方联盟激活了一场圣地战,并将于20分钟后结束,请做好出兵准备！",
+	en:"Your Alliance has activated a Shrine event, and it will end in 20 minutes, go for the war!",
 	tw:"我方聯盟激活了一場聖地戰,並將於20分鐘後結束,請做好出兵準備！"
 }
 alliance["CityBeAttacked"] = {
 	key:"CityBeAttacked",
 	cn:"主人，你的城市即将被攻打！",
-	en:"这是英文:主人，你的城市即将被攻打！",
+	en:"Your city is under attack!",
 	tw:"主人，你的城市即將被攻打！"
 }
 alliance["AllianceMovedTitle"] = {
 	key:"AllianceMovedTitle",
 	cn:"你的联盟领地已被迁移",
-	en:"你的联盟领地已被迁移",
+	en:"Your Alliance has been migrated",
 	tw:"你的聯盟領地已被遷移"
 }
 alliance["AllianceMovedContent"] = {
 	key:"AllianceMovedContent",
 	cn:"你所在的联盟领地已由第%d圈迁移至第%d圈",
-	en:"这是英文:你所在的联盟领地已由第%d圈迁移至第%d圈",
+	en:"Your Alliance territory has been migrated from round%s to round%s.",
 	tw:"你所在的聯盟領地已由第%d圈遷移至第%d圈"
 }
 alliance["AllianceFightTitle"] = {
 	key:"AllianceFightTitle",
 	cn:"联盟会战结果",
-	en:"联盟会战结果",
+	en:"Alliance Battle result",
 	tw:"聯盟會戰結果"
 }
 alliance["AllianceFightSuccess"] = {
 	key:"AllianceFightSuccess",
 	cn:"你的联盟在联盟会战中战胜了[%s]%s联盟",
-	en:"这是英文:你的联盟在联盟会战中战胜了[%s]%s联盟",
+	en:"Your Alliance has defeated [%s]%s in the last Alliance Battle.",
 	tw:"你的聯盟在聯盟會戰中戰勝了[%s]%s聯盟"
 }
 alliance["AllianceFightFailed"] = {
 	key:"AllianceFightFailed",
 	cn:"你的联盟在联盟会战中落败于[%s]%s联盟",
-	en:"这是英文:你的联盟在联盟会战中落败于[%s]%s联盟",
+	en:"[%s]%s has defeated your Alliance in the last Alliance Battle.",
 	tw:"你的聯盟在聯盟會戰中落敗於[%s]%s聯盟"
 }
 alliance["AttackMissTitle"] = {
 	key:"AttackMissTitle",
 	cn:"行军失败",
-	en:"行军失败",
+	en:"March failed",
 	tw:"行軍失敗"
 }
 alliance["AttackMissContent"] = {
 	key:"AttackMissContent",
 	cn:"[%s]%s(%d,%d)已不存在（迁移或消失），部队已原路返回",
-	en:"[%s]%s(%d,%d)已不存在（迁移或消失），部队已原路返回",
+	en:"[%s]%s(%d,%d) does not existed(migrated or disappeared), your troop is returning now.",
 	tw:"[%s]%s(%d,%d)已不存在（遷移或消失），部隊已原路返回"
 }
 alliance["AttackProtectedTitle"] = {
 	key:"AttackProtectedTitle",
 	cn:"行军失败",
-	en:"行军失败",
+	en:"March failed",
 	tw:"行軍失敗"
 }
 alliance["AttackProtectedContent"] = {
 	key:"AttackProtectedContent",
 	cn:"[%s]%s(%d,%d)正处于保护状态，无法攻打，部队已原路返回",
-	en:"[%s]%s(%d,%d)正处于保护状态，无法攻打，部队已原路返回",
+	en:"You can't attack [%s]%s(%d,%d) who is under protection, your troop is returning now.",
 	tw:"[%s]%s(%d,%d)正處於保護狀態，無法攻打，部隊已原路返回"
 }
 alliance["CollectFailedTitle"] = {
 	key:"CollectFailedTitle",
 	cn:"行军失败",
-	en:"行军失败",
+	en:"March failed",
 	tw:"行軍失敗"
 }
 alliance["CollectFailedContent"] = {
 	key:"CollectFailedContent",
 	cn:"[%s]%s(%d,%d)已被盟友占领，部队已原路返回",
-	en:"[%s]%s(%d,%d)已被盟友占领，部队已原路返回",
+	en:"[%s]%s(%d,%d) is already occupied by your ally, your troop is returning now.",
 	tw:"[%s]%s(%d,%d)已被盟友占領，部隊已原路返回"
 }
 alliance["HelpDefenceFailedTitle"] = {
 	key:"HelpDefenceFailedTitle",
 	cn:"行军失败",
-	en:"行军失败",
+	en:"March failed",
 	tw:"行軍失敗"
 }
 alliance["HelpDefenceFailedContent"] = {
 	key:"HelpDefenceFailedContent",
 	cn:"[%s]%s(%d,%d)协防数量已达上限，无法继续协防，部队已原路返回",
-	en:"[%s]%s(%d,%d)协防数量已达上限，无法继续协防，部队已原路返回",
+	en:"You can't help defending [%s]%s(%d,%d) who is full of assist, your troop is returning now.",
 	tw:"[%s]%s(%d,%d)協防數量已達上限，無法繼續協防，部隊已原路返回"
 }
