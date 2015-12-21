93047ba02033806ab3856cf220c044824c8002ee
Modun Zhang <modunzhang@gmail.com>
2015-12-21 11:59:20 +0800

    调整配置文件

--- game-server/app/datas/Gacha_normal.js
+++ game-server/app/datas/Gacha_normal.js
@@ -7,95 +7,95 @@ normal[1] = {
 	index:1,
 	itemName:"gemClass_2",
 	itemCount:1,
-	weight:5
+	weight:10
 }
 normal[2] = {
 	index:2,
 	itemName:"gemClass_2",
 	itemCount:5,
-	weight:1
+	weight:2
 }
 normal[3] = {
 	index:3,
 	itemName:"woodClass_3",
 	itemCount:3,
-	weight:10
+	weight:110
 }
 normal[4] = {
 	index:4,
 	itemName:"woodClass_4",
 	itemCount:1,
-	weight:5
+	weight:52
 }
 normal[5] = {
 	index:5,
 	itemName:"stoneClass_3",
 	itemCount:3,
-	weight:10
+	weight:110
 }
 normal[6] = {
 	index:6,
 	itemName:"stoneClass_4",
 	itemCount:1,
-	weight:5
+	weight:52
 }
 normal[7] = {
 	index:7,
 	itemName:"ironClass_3",
 	itemCount:3,
-	weight:10
+	weight:110
 }
 normal[8] = {
 	index:8,
 	itemName:"ironClass_4",
 	itemCount:1,
-	weight:5
+	weight:52
 }
 normal[9] = {
 	index:9,
 	itemName:"foodClass_3",
 	itemCount:3,
-	weight:10
+	weight:110
 }
 normal[10] = {
 	index:10,
 	itemName:"foodClass_4",
 	itemCount:1,
-	weight:5
+	weight:52
 }
 normal[11] = {
 	index:11,
 	itemName:"coinClass_3",
 	itemCount:3,
-	weight:5
+	weight:50
 }
 normal[12] = {
 	index:12,
 	itemName:"casinoTokenClass_1",
 	itemCount:1,
-	weight:2
+	weight:20
 }
 normal[13] = {
 	index:13,
 	itemName:"speedup_3",
 	itemCount:3,
-	weight:10
+	weight:100
 }
 normal[14] = {
 	index:14,
 	itemName:"speedup_4",
 	itemCount:1,
-	weight:5
+	weight:50
 }
 normal[15] = {
 	index:15,
 	itemName:"vipActive_3",
 	itemCount:1,
-	weight:6
+	weight:60
 }
 normal[16] = {
 	index:16,
 	itemName:"warSpeedupClass_1",
 	itemCount:1,
-	weight:6
+	weight:60
 }
