233e47134277867fcc1c7e4156c2fc88f93a7178
Modun Zhang <modunzhang@gmail.com>
2015-12-22 10:57:19 +0800

    调整配置文件

--- game-server/app/datas/Activities_day14.js
+++ game-server/app/datas/Activities_day14.js
@@ -5,43 +5,43 @@ module.exports = day14
 
 day14[1] = {
 	day:1,
-	rewards:"soldiers:catapult_1:1:200"
+	rewards:"soldiers:catapult_1:200"
 }
 day14[2] = {
 	day:2,
-	rewards:"soldiers:lancer_1:1:200"
+	rewards:"soldiers:lancer_1:200"
 }
 day14[3] = {
 	day:3,
-	rewards:"soldiers:sentinel_1:1:300"
+	rewards:"soldiers:sentinel_1:300"
 }
 day14[4] = {
 	day:4,
-	rewards:"soldiers:crossbowman_1:1:300"
+	rewards:"soldiers:crossbowman_1:300"
 }
 day14[5] = {
 	day:5,
-	rewards:"soldiers:horseArcher_1:1:200"
+	rewards:"soldiers:horseArcher_1:200"
 }
 day14[6] = {
 	day:6,
-	rewards:"soldiers:ballista_1:1:200"
+	rewards:"soldiers:ballista_1:200"
 }
 day14[7] = {
 	day:7,
-	rewards:"soldiers:swordsman_2:1:200"
+	rewards:"soldiers:swordsman_2:200"
 }
 day14[8] = {
 	day:8,
-	rewards:"soldiers:ranger_2:1:200"
+	rewards:"soldiers:ranger_2:200"
 }
 day14[9] = {
 	day:9,
-	rewards:"soldiers:lancer_2:1:200"
+	rewards:"soldiers:lancer_2:200"
 }
 day14[10] = {
 	day:10,
-	rewards:"soldiers:catapult_2:1:200"
+	rewards:"soldiers:catapult_2:200"
 }
 day14[11] = {
 	day:11,
