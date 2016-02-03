cce33b09355950bd73a086c6fe2427b6035f25cf
Modun Zhang <modunzhang@gmail.com>
2016-02-03 14:33:40 +0800

    调整配置文件

--- game-server/app/datas/StoreItems_items.js
+++ game-server/app/datas/StoreItems_items.js
@@ -10,7 +10,9 @@ items[1] = {
 	price:4.990000,
 	desc:"新手大礼包",
 	gem:2500,
-	rewards:"special:dragonChest_1:5,resource:woodClass_4:2,resource:stoneClass_4:2,resource:ironClass_4:2,resource:foodClass_4:2,resource:coinClass_4:2,speedup:speedup_1:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2"
+	rewards:"resource:gemClass_2:25,special:redbag_1:5,special:dragonChest_1:5,resource:woodClass_4:2,resource:stoneClass_4:2,resource:ironClass_4:2,resource:foodClass_4:2,resource:coinClass_4:2,speedup:speedup_1:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2
+
+"
 }
 items[2] = {
 	order:2,
@@ -19,7 +21,7 @@ items[2] = {
 	price:9.990000,
 	desc:"探险家礼包",
 	gem:5500,
-	rewards:"special:dragonChest_2:2,special:dragonChest_1:5,resource:woodClass_5:2,resource:stoneClass_5:2,resource:ironClass_5:2,resource:foodClass_5:2,resource:coinClass_5:2,speedup:speedup_1:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2,speedup:speedup_5:2",
+	rewards:"resource:gemClass_2:55,special:redbag_2:5,special:redbag_1:5,special:dragonChest_2:2,special:dragonChest_1:5,resource:woodClass_5:2,resource:stoneClass_5:2,resource:ironClass_5:2,resource:foodClass_5:2,resource:coinClass_5:2,speedup:speedup_1:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2,speedup:speedup_5:2",
 	allianceRewards:"special:chest_1:1"
 }
 items[3] = {
@@ -29,7 +31,7 @@ items[3] = {
 	price:19.990000,
 	desc:"铁血军团礼包",
 	gem:12000,
-	rewards:"special:dragonChest_2:5,special:dragonChest_1:10,resource:woodClass_6:2,resource:stoneClass_6:2,resource:ironClass_6:2,resource:foodClass_6:2,resource:coinClass_6:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2,speedup:speedup_5:2,speedup:speedup_6:2",
+	rewards:"resource:gemClass_3:12,special:redbag_3:5,special:redbag_2:5,special:redbag_1:5,special:dragonChest_2:5,special:dragonChest_1:10,resource:woodClass_6:2,resource:stoneClass_6:2,resource:ironClass_6:2,resource:foodClass_6:2,resource:coinClass_6:2,speedup:speedup_2:2,speedup:speedup_3:2,speedup:speedup_4:2,speedup:speedup_5:2,speedup:speedup_6:2",
 	allianceRewards:"special:chest_2:1"
 }
 items[4] = {
@@ -39,7 +41,7 @@ items[4] = {
 	price:49.990000,
 	desc:"帝国荣耀礼包",
 	gem:35000,
-	rewards:"special:dragonChest_3:5,special:dragonChest_2:5,special:dragonChest_1:20,resource:woodClass_6:5,resource:stoneClass_6:5,resource:ironClass_6:5,resource:foodClass_6:5,resource:coinClass_6:5,speedup:speedup_2:5,speedup:speedup_3:5,speedup:speedup_4:5,speedup:speedup_5:5,speedup:speedup_6:5,buff:woodBonus_1:5,buff:stoneBonus_1:5,buff:ironBonus_1:5,buff:foodBonus_1:5,buff:coinBonus_1:5",
+	rewards:"resource:gemClass_3:35,special:redbag_3:10,special:redbag_2:15,special:redbag_1:15,special:dragonChest_3:5,special:dragonChest_2:5,special:dragonChest_1:20,resource:woodClass_6:5,resource:stoneClass_6:5,resource:ironClass_6:5,resource:foodClass_6:5,resource:coinClass_6:5,speedup:speedup_2:5,speedup:speedup_3:5,speedup:speedup_4:5,speedup:speedup_5:5,speedup:speedup_6:5,buff:woodBonus_1:5,buff:stoneBonus_1:5,buff:ironBonus_1:5,buff:foodBonus_1:5,buff:coinBonus_1:5",
 	allianceRewards:"special:chest_3:1"
 }
 items[5] = {
@@ -49,6 +51,6 @@ items[5] = {
 	price:99.990000,
 	desc:"龙族王朝礼包",
 	gem:80000,
-	rewards:"special:dragonChest_3:10,special:dragonChest_2:20,special:dragonChest_1:30,resource:woodClass_7:5,resource:stoneClass_7:5,resource:ironClass_7:5,resource:foodClass_7:5,resource:coinClass_7:5,speedup:speedup_3:5,speedup:speedup_4:5,speedup:speedup_5:5,speedup:speedup_6:5,speedup:speedup_7:5,buff:woodBonus_1:5,buff:stoneBonus_1:5,buff:ironBonus_1:5,buff:foodBonus_1:5,buff:coinBonus_1:5,buff:unitHpBonus_1:5,buff:infantryAtkBonus_1:5,buff:archerAtkBonus_1:5,buff:cavalryAtkBonus_1:5,buff:siegeAtkBonus_1:5",
+	rewards:"resource:gemClass_3:80,special:redbag_3:30,special:redbag_2:30,special:redbag_1:30,special:dragonChest_3:10,special:dragonChest_2:20,special:dragonChest_1:30,resource:woodClass_7:5,resource:stoneClass_7:5,resource:ironClass_7:5,resource:foodClass_7:5,resource:coinClass_7:5,speedup:speedup_3:5,speedup:speedup_4:5,speedup:speedup_5:5,speedup:speedup_6:5,speedup:speedup_7:5,buff:woodBonus_1:5,buff:stoneBonus_1:5,buff:ironBonus_1:5,buff:foodBonus_1:5,buff:coinBonus_1:5,buff:unitHpBonus_1:5,buff:infantryAtkBonus_1:5,buff:archerAtkBonus_1:5,buff:cavalryAtkBonus_1:,buff:siegeAtkBonus_1:5",
 	allianceRewards:"special:chest_4:1"
 }
