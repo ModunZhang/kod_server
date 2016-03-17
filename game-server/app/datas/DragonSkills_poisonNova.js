997e89fc92f32c4f5eecd0d74392c22ead4c5439
Modun Zhang <modunzhang@gmail.com>
2016-03-18 00:34:24 +0800

    调整配置文件

--- game-server/app/datas/DragonSkills_poisonNova.js
+++ game-server/app/datas/DragonSkills_poisonNova.js
@@ -6,100 +6,100 @@ module.exports = poisonNova
 poisonNova[1] = {
 	level:1,
 	effect:0.080000,
-	bloodCost:40
+	bloodCost:280
 }
 poisonNova[2] = {
 	level:2,
 	effect:0.090000,
-	bloodCost:100
+	bloodCost:680
 }
 poisonNova[3] = {
 	level:3,
 	effect:0.100000,
-	bloodCost:180
+	bloodCost:1160
 }
 poisonNova[4] = {
 	level:4,
 	effect:0.110000,
-	bloodCost:260
+	bloodCost:1680
 }
 poisonNova[5] = {
 	level:5,
 	effect:0.120000,
-	bloodCost:340
+	bloodCost:2280
 }
 poisonNova[6] = {
 	level:6,
 	effect:0.130000,
-	bloodCost:840
+	bloodCost:5560
 }
 poisonNova[7] = {
 	level:7,
 	effect:0.140000,
-	bloodCost:1220
+	bloodCost:8080
 }
 poisonNova[8] = {
 	level:8,
 	effect:0.150000,
-	bloodCost:1620
+	bloodCost:10800
 }
 poisonNova[9] = {
 	level:9,
 	effect:0.160000,
-	bloodCost:2080
+	bloodCost:13840
 }
 poisonNova[10] = {
 	level:10,
 	effect:0.170000,
-	bloodCost:2580
+	bloodCost:17080
 }
 poisonNova[11] = {
 	level:11,
 	effect:0.180000,
-	bloodCost:6660
+	bloodCost:44320
 }
 poisonNova[12] = {
 	level:12,
 	effect:0.190000,
-	bloodCost:9880
+	bloodCost:65800
 }
 poisonNova[13] = {
 	level:13,
 	effect:0.200000,
-	bloodCost:13720
+	bloodCost:91400
 }
 poisonNova[14] = {
 	level:14,
 	effect:0.210000,
-	bloodCost:18180
+	bloodCost:121120
 }
 poisonNova[15] = {
 	level:15,
 	effect:0.220000,
-	bloodCost:23240
+	bloodCost:154880
 }
 poisonNova[16] = {
 	level:16,
 	effect:0.230000,
-	bloodCost:44240
+	bloodCost:294920
 }
 poisonNova[17] = {
 	level:17,
 	effect:0.240000,
-	bloodCost:60220
+	bloodCost:401440
 }
 poisonNova[18] = {
 	level:18,
 	effect:0.250000,
-	bloodCost:78660
+	bloodCost:524320
 }
 poisonNova[19] = {
 	level:19,
 	effect:0.260000,
-	bloodCost:99540
+	bloodCost:663560
 }
 poisonNova[20] = {
 	level:20,
 	effect:0.270000,
-	bloodCost:122880
+	bloodCost:819200
 }
