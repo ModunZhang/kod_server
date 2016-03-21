7a93c5be70dcc86fa2186eb900564ec8745d6bbd
Modun Zhang <modunzhang@gmail.com>
2016-03-21 15:06:11 +0800

    Bug修复

--- game-server/app/services/playerApiService3.js
+++ game-server/app/services/playerApiService3.js
@@ -996,11 +996,12 @@ pro.getPveStageReward = function(playerId, stageName, callback){
 	var lockPairs = [];
 	var updateFuncs = [];
 	var stageIndex = null;
+	var rewardIndex = null;
 	this.cacheService.findPlayerAsync(playerId).then(function(doc){
 		playerDoc = doc;
 		var stageParams = stageName.split('_');
 		stageIndex = parseInt(stageParams[0]) - 1;
-		var rewardIndex = parseInt(stageParams[1]);
+		rewardIndex = parseInt(stageParams[1]);
 		if(!_.isObject(playerDoc.pve[stageIndex])) return Promise.reject(ErrorUtils.canNotGetPvEStarRewardyet(playerId, stageName));
 		var rewardedIndex = _.find(playerDoc.pve[stageIndex].rewarded, function(rewardedIndex){
 			return rewardedIndex == rewardIndex;
