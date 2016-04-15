6aea12ba7859dccbf0ebbf7889b32ce3feda2b08
Modun Zhang <modunzhang@gmail.com>
2016-04-15 15:52:08 +0800

    修改配置文件

--- game-server/app/datas/StoreItems_items.js
+++ game-server/app/datas/StoreItems_items.js
@@ -9,7 +9,7 @@ items[1] = {
 	productId:"com.dragonfall.2500dragoncoins",
 	price:4.990000,
 	desc:"新手大礼包",
-	gem:5000,
+	gem:2500,
 	rewards:"items:dragonChest_1:5,items:woodClass_4:2,items:stoneClass_4:2,items:ironClass_4:2,items:foodClass_4:2,items:coinClass_4:2,items:speedup_1:2,items:speedup_2:2,items:speedup_3:2,items:speedup_4:2"
 }
 items[2] = {
@@ -18,7 +18,7 @@ items[2] = {
 	productId:"com.dragonfall.5500dragoncoins",
 	price:9.990000,
 	desc:"探险家礼包",
-	gem:11000,
+	gem:5500,
 	rewards:"items:dragonChest_2:2,items:dragonChest_1:5,items:woodClass_5:2,items:stoneClass_5:2,items:ironClass_5:2,items:foodClass_5:2,items:coinClass_5:2,items:speedup_1:2,items:speedup_2:2,items:speedup_3:2,items:speedup_4:2,items:speedup_5:2",
 	allianceRewards:"items:chest_1:1"
 }
@@ -28,7 +28,7 @@ items[3] = {
 	productId:"com.dragonfall.12000dragoncoins",
 	price:19.990000,
 	desc:"铁血军团礼包",
-	gem:24000,
+	gem:12000,
 	rewards:"items:dragonChest_2:5,items:dragonChest_1:10,items:woodClass_6:2,items:stoneClass_6:2,items:ironClass_6:2,items:foodClass_6:2,items:coinClass_6:2,items:speedup_2:2,items:speedup_3:2,items:speedup_4:2,items:speedup_5:2,items:speedup_6:2",
 	allianceRewards:"items:chest_2:1"
 }
@@ -38,7 +38,7 @@ items[4] = {
 	productId:"com.dragonfall.35000dragoncoins",
 	price:49.990000,
 	desc:"帝国荣耀礼包",
-	gem:70000,
+	gem:35000,
 	rewards:"items:dragonChest_3:5,items:dragonChest_2:5,items:dragonChest_1:20,items:woodClass_6:5,items:stoneClass_6:5,items:ironClass_6:5,items:foodClass_6:5,items:coinClass_6:5,items:speedup_2:5,items:speedup_3:5,items:speedup_4:5,items:speedup_5:5,items:speedup_6:5,items:woodBonus_1:5,items:stoneBonus_1:5,items:ironBonus_1:5,items:foodBonus_1:5,items:coinBonus_1:5",
 	allianceRewards:"items:chest_3:1"
 }
@@ -48,7 +48,7 @@ items[5] = {
 	productId:"com.dragonfall.80000dragoncoins",
 	price:99.990000,
 	desc:"龙族王朝礼包",
-	gem:160000,
+	gem:80000,
 	rewards:"items:dragonChest_3:10,items:dragonChest_2:20,items:dragonChest_1:30,items:woodClass_7:5,items:stoneClass_7:5,items:ironClass_7:5,items:foodClass_7:5,items:coinClass_7:5,items:speedup_3:5,items:speedup_4:5,items:speedup_5:5,items:speedup_6:5,items:speedup_7:5,items:woodBonus_1:5,items:stoneBonus_1:5,items:ironBonus_1:5,items:foodBonus_1:5,items:coinBonus_1:5,items:unitHpBonus_1:5,items:infantryAtkBonus_1:5,items:archerAtkBonus_1:5,items:cavalryAtkBonus_1:5,items:siegeAtkBonus_1:5",
 	allianceRewards:"items:chest_4:1"
 }
