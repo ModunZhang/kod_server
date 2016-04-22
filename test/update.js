d83279ae72190c94d702ea475410010775763014
Modun Zhang <modunzhang@gmail.com>
2016-04-22 09:51:12 +0800

    日报脚本更新

--- test/update.js
+++ test/update.js
@@ -215,7 +215,7 @@ var dbBatcatIos = 'mongodb://114.55.60.126:27017/dragonfall-batcat-ios'
 var dbAiyingyongAndroid = 'mongodb://47.88.195.9:27017/dragonfall-aiyingyong-android'
 
 mongoose.connect(dbAiyingyongAndroid, function(){
-	Analyse('2016-04-20').then(function(data){
+	Analyse('2016-04-21').then(function(data){
 		console.log(data);
 		mongoose.disconnect();
 	})
