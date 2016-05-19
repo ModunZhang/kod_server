c287cf1ef2af4eb5185e6f9f5e6c473d2dd3e18c
Modun Zhang <modunzhang@gmail.com>
2016-05-19 16:20:52 +0800

    调整配置文件

--- game-server/app/datas/StoreItems_promotionItems.js
+++ game-server/app/datas/StoreItems_promotionItems.js
@@ -8,9 +8,10 @@ promotionItems[1] = {
 	name:"promotion_product_1_1",
 	productId:"com.dragonfall.promotion.15000dragoncoins",
 	price:9.990000,
+	normalPrice:55.550000,
 	desc:"远古学识特惠",
 	gem:15000,
-	promotionPercent:6.060000,
+	promotionPercent:455.500000,
 	existHours:36.000000,
 	rewards:"special:dragonExp_2:5,special:dragonExp_1:10,resource:woodClass_4:10,resource:stoneClass_4:10,resource:ironClass_4:10,resource:foodClass_4:10,resource:woodClass_3:50,resource:stoneClass_3:50,resource:ironClass_3:50,resource:foodClass_3:50,speedup:speedup_1:50,speedup:speedup_2:20,speedup:speedup_3:10"
 }
@@ -19,9 +20,10 @@ promotionItems[2] = {
 	name:"promotion_product_1_2",
 	productId:"com.dragonfall.promotion.20000dragoncoins",
 	price:9.990000,
+	normalPrice:56.450000,
 	desc:"富饶之地特惠",
 	gem:20000,
-	promotionPercent:6.650000,
+	promotionPercent:464.500000,
 	existHours:24.000000,
 	rewards:"resource:woodClass_6:4,resource:stoneClass_6:4,resource:ironClass_6:4,resource:foodClass_6:4,resource:woodClass_4:20,resource:stoneClass_4:20,resource:ironClass_4:20,resource:foodClass_4:20,speedup:speedup_1:50,speedup:speedup_2:20,speedup:speedup_3:20,speedup:speedup_4:10,speedup:speedup_5:5"
 }
@@ -30,9 +32,10 @@ promotionItems[3] = {
 	name:"promotion_product_2_1",
 	productId:"com.dragonfall.promotion.30000dragoncoins",
 	price:19.990000,
+	normalPrice:115.800000,
 	desc:"龙族宝藏特惠",
 	gem:30000,
-	promotionPercent:6.290000,
+	promotionPercent:479.000000,
 	existHours:48.000000,
 	rewards:"special:dragonChest_3:5,special:dragonChest_2:20,special:dragonChest_1:10,special:dragonExp_1:5,special:sweepScroll:100,buff:masterOfDefender_2:5,speedup:warSpeedupClass_1:20,speedup:warSpeedupClass_2:10,resource:woodClass_6:1,resource:stoneClass_6:1,resource:ironClass_6:1,resource:foodClass_6:1,speedup:speedup_3:20"
 }
@@ -41,9 +44,10 @@ promotionItems[4] = {
 	name:"promotion_product_2_2",
 	productId:"com.dragonfall.promotion.40000dragoncoins",
 	price:19.990000,
+	normalPrice:116.650000,
 	desc:"好运连连特惠",
 	gem:40000,
-	promotionPercent:6.830000,
+	promotionPercent:483.250000,
 	existHours:12.000000,
 	rewards:"resource:casinoTokenClass_5:2,special:chest_4:2,special:chest_3:5,special:chest_2:10,special:chest_1:15,special:sweepScroll:50,resource:woodClass_6:8,resource:stoneClass_6:8,resource:ironClass_6:8,resource:foodClass_6:8,speedup:speedup_4:10,speedup:speedup_5:5,speedup:speedup_6:2"
 }
@@ -52,9 +56,10 @@ promotionItems[5] = {
 	name:"promotion_product_3_1",
 	productId:"com.dragonfall.promotion.80000dragoncoins",
 	price:49.990000,
+	normalPrice:302.600000,
 	desc:"贵族传承特惠",
 	gem:80000,
-	promotionPercent:6.650000,
+	promotionPercent:505.200000,
 	existHours:24.000000,
 	rewards:"special:vipPoint_4:8,special:vipActive_3:30,special:dragonExp_1:10,special:chest_4:5,special:chest_3:5,resource:woodClass_6:5,resource:stoneClass_6:5,resource:ironClass_6:5,resource:foodClass_6:5,special:sweepScroll:30,speedup:speedup_4:20,speedup:speedup_3:50,speedup:speedup_2:100"
 }
@@ -63,9 +68,10 @@ promotionItems[6] = {
 	name:"promotion_product_3_2",
 	productId:"com.dragonfall.promotion.70000dragoncoins",
 	price:49.990000,
+	normalPrice:304.650000,
 	desc:"英雄之血特惠",
 	gem:70000,
-	promotionPercent:6.490000,
+	promotionPercent:509.300000,
 	existHours:36.000000,
 	rewards:"special:heroBlood_3:8,special:dragonExp_1:5,special:dragonHp_2:10,buff:marchSpeedBonus_1:5,buff:unitHpBonus_1:5,buff:infantryAtkBonus_1:5,buff:archerAtkBonus_1:5,buff:cavalryAtkBonus_1:5,buff:siegeAtkBonus_1:5,speedup:warSpeedupClass_1:20,speedup:warSpeedupClass_2:10,resource:coinClass_5:10,special:sweepScroll:50"
 }
@@ -74,9 +80,10 @@ promotionItems[7] = {
 	name:"promotion_product_4_1",
 	productId:"com.dragonfall.promotion.150000dragoncoins",
 	price:99.990000,
+	normalPrice:653.400000,
 	desc:"不死军团特惠",
 	gem:150000,
-	promotionPercent:7.034000,
+	promotionPercent:553.400000,
 	existHours:12.000000,
 	rewards:"skeletonWarrior:2400,skeletonArcher:2400,deathKnight:1200,meatWagon:1200,special:dragonChest_3:20,special:dragonChest_2:20,special:dragonExp_2:10,special:dragonExp_1:20,speedup:warSpeedupClass_2:100,resource:woodClass_6:10,resource:stoneClass_6:10,resource:ironClass_6:10,resource:foodClass_6:10"
 }
@@ -85,9 +92,10 @@ promotionItems[8] = {
 	name:"promotion_product_4_2",
 	productId:"com.dragonfall.promotion.200000dragoncoins",
 	price:99.990000,
+	normalPrice:660.500000,
 	desc:"金龙币特惠",
 	gem:200000,
-	promotionPercent:7.605000,
+	promotionPercent:560.500000,
 	existHours:48.000000,
 	rewards:"resource:gemClass_3:100,resource:gemClass_2:200,resource:coinClass_6:20,resource:woodClass_6:40,resource:stoneClass_6:40,resource:ironClass_6:40,resource:foodClass_6:40,speedup:speedup_8:10,speedup:speedup_7:20,speedup:speedup_6:50,speedup:speedup_5:50,speedup:speedup_4:100,speedup:speedup_3:100"
 }
