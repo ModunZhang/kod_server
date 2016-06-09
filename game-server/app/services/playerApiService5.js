6224e9b57ad21b877f55ecd134df1ccea93dd8f9
Modun Zhang <modunzhang@gmail.com>
2016-06-10 00:43:32 +0800

    代码优化

--- game-server/app/services/playerApiService5.js
+++ game-server/app/services/playerApiService5.js
@@ -759,7 +759,7 @@ pro.getPlayerActivityRankRewards = function(playerId, activityType, callback){
 		var items = DataUtils.getActivityRankRewards(activityType, myRank);
 		activity.rankRewardsGeted = true;
 		playerData.push(["activities." + activityType + '.rankRewardsGeted', true]);
-		updateFuncs.push([self.dataService, self.dataService.addPlayerItemsAsync, playerDoc, playerData, 'getPlayerActivityRankRewards', {activityType:activityType}, items])
+		updateFuncs.push([self.dataService, self.dataService.addPlayerItemsAsync, playerDoc, playerData, 'getPlayerActivityRankRewards', {activityType:activityType, myRank:myRank}, items])
 	}).then(function(){
 		return LogicUtils.excuteAll(updateFuncs)
 	}).then(function(){
