c8cdc6114031274fc625deb93fa842bca71c6c59
Modun Zhang <modunzhang@gmail.com>
2016-06-20 17:53:02 +0800

    调整配置文件

--- game-server/app/datas/Localizations_player.js
+++ game-server/app/datas/Localizations_player.js
@@ -11,8 +11,8 @@ player["MuteTitle"] = {
 }
 player["MuteContent"] = {
 	key:"MuteContent",
-	cn:"你被MOD[%s]禁言%d分钟,禁言原因:%s。 ",
-	en:"You are muted for %d minutes by MOD[%s], reason: %s. ",
+	cn:"你被MOD-%s禁言%d分钟,禁言原因:%s。 ",
+	en:"You are muted for %d minutes by MOD-%s, reason: %s. ",
 	tw:"你被墨子[%s]禁言%d分钟,禁言原因:%s"
 }
 player["UnMuteTitle"] = {
@@ -30,6 +30,6 @@ player["UnMuteContent"] = {
 player["ChatMuteNotice"] = {
 	key:"ChatMuteNotice",
 	cn:"[%s]被MOD[%s]禁言%d分钟,禁言原因:%s。 ",
-	en:"[%s] is muted by MOD[%s] for %d minutes , reason: %s. ",
+	en:"%s is muted by MOD-%s for %d minutes, reason: %s. ",
 	tw:"[%s]被墨子[%s]禁言%d分钟,禁言原因:%s"
 }
