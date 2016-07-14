30ffbfbe1855e9acd836b1185a40e1fa720b9dbb
Modun Zhang <modunzhang@gmail.com>
2016-07-14 21:08:22 +0800

    修复删除邮件Bug

--- game-server/app/services/playerApiService3.js
+++ game-server/app/services/playerApiService3.js
@@ -154,7 +154,7 @@ pro.deleteMails = function(playerId, mailIds, callback){
 		for(var i = 0; i < mailIds.length; i++){
 			(function(){
 				var mail = LogicUtils.getPlayerMailById(playerDoc, mailIds[i])
-				if(!_.isObject(mail)) return;
+				if(!_.isObject(mail) || mail.isSaved) return;
 				playerData.push(["mails." + playerDoc.mails.indexOf(mail), null])
 				LogicUtils.removeItemInArray(playerDoc.mails, mail)
 			})()
