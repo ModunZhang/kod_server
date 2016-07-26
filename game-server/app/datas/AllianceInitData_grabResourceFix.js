0318debafd9dde420ba2635568aa23ae7124e032
Modun Zhang <modunzhang@gmail.com>
2016-07-26 12:26:39 +0800

    debug

--- game-server/app/datas/AllianceInitData_grabResourceFix.js
+++ game-server/app/datas/AllianceInitData_grabResourceFix.js
@@ -5,101 +5,96 @@ module.exports = grabResourceFix
 
 grabResourceFix[0] = {
 	index:0,
-	powerComparison:0.000000,
+	powerCompare:1.000000,
 	grabPercentFix:1.000000
 }
 grabResourceFix[1] = {
 	index:1,
-	powerComparison:1.000000,
+	powerCompare:1.500000,
 	grabPercentFix:1.000000
 }
 grabResourceFix[2] = {
 	index:2,
-	powerComparison:1.500000,
-	grabPercentFix:1.000000
+	powerCompare:2.000000,
+	grabPercentFix:0.950000
 }
 grabResourceFix[3] = {
 	index:3,
-	powerComparison:2.000000,
-	grabPercentFix:0.950000
+	powerCompare:2.500000,
+	grabPercentFix:0.900000
 }
 grabResourceFix[4] = {
 	index:4,
-	powerComparison:2.500000,
-	grabPercentFix:0.900000
+	powerCompare:3.000000,
+	grabPercentFix:0.850000
 }
 grabResourceFix[5] = {
 	index:5,
-	powerComparison:3.000000,
-	grabPercentFix:0.850000
+	powerCompare:3.500000,
+	grabPercentFix:0.800000
 }
 grabResourceFix[6] = {
 	index:6,
-	powerComparison:3.500000,
-	grabPercentFix:0.800000
+	powerCompare:4.000000,
+	grabPercentFix:0.750000
 }
 grabResourceFix[7] = {
 	index:7,
-	powerComparison:4.000000,
-	grabPercentFix:0.750000
+	powerCompare:4.500000,
+	grabPercentFix:0.700000
 }
 grabResourceFix[8] = {
 	index:8,
-	powerComparison:4.500000,
-	grabPercentFix:0.700000
+	powerCompare:5.000000,
+	grabPercentFix:0.650000
 }
 grabResourceFix[9] = {
 	index:9,
-	powerComparison:5.000000,
-	grabPercentFix:0.650000
+	powerCompare:5.500000,
+	grabPercentFix:0.600000
 }
 grabResourceFix[10] = {
 	index:10,
-	powerComparison:5.500000,
-	grabPercentFix:0.600000
+	powerCompare:6.000000,
+	grabPercentFix:0.550000
 }
 grabResourceFix[11] = {
 	index:11,
-	powerComparison:6.000000,
-	grabPercentFix:0.550000
+	powerCompare:6.500000,
+	grabPercentFix:0.500000
 }
 grabResourceFix[12] = {
 	index:12,
-	powerComparison:6.500000,
-	grabPercentFix:0.500000
+	powerCompare:7.000000,
+	grabPercentFix:0.450000
 }
 grabResourceFix[13] = {
 	index:13,
-	powerComparison:7.000000,
-	grabPercentFix:0.450000
+	powerCompare:7.500000,
+	grabPercentFix:0.400000
 }
 grabResourceFix[14] = {
 	index:14,
-	powerComparison:7.500000,
-	grabPercentFix:0.400000
+	powerCompare:8.000000,
+	grabPercentFix:0.350000
 }
 grabResourceFix[15] = {
 	index:15,
-	powerComparison:8.000000,
-	grabPercentFix:0.350000
+	powerCompare:8.500000,
+	grabPercentFix:0.300000
 }
 grabResourceFix[16] = {
 	index:16,
-	powerComparison:8.500000,
-	grabPercentFix:0.300000
+	powerCompare:9.000000,
+	grabPercentFix:0.250000
 }
 grabResourceFix[17] = {
 	index:17,
-	powerComparison:9.000000,
-	grabPercentFix:0.250000
+	powerCompare:9.500000,
+	grabPercentFix:0.200000
 }
 grabResourceFix[18] = {
 	index:18,
-	powerComparison:9.500000,
-	grabPercentFix:0.200000
-}
-grabResourceFix[19] = {
-	index:19,
-	powerComparison:10.000000,
+	powerCompare:10.000000,
 	grabPercentFix:0.150000
 }
