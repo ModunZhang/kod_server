9498808d0ad9fb50a1c77091d4c22a28111e52a4
Modun Zhang <modunzhang@gmail.com>
2016-08-02 16:36:21 +0800

    调整配置文件

---
+++ game-server/app/datas/AllianceInitData_allianceFightLoyaltyGet.js
@@ -0,0 +1,45 @@
+"use strict"
+
+var allianceFightLoyaltyGet = []
+module.exports = allianceFightLoyaltyGet
+
+allianceFightLoyaltyGet[1] = {
+	killRank:1,
+	loyaltyPercent:0.200000
+}
+allianceFightLoyaltyGet[2] = {
+	killRank:2,
+	loyaltyPercent:0.170000
+}
+allianceFightLoyaltyGet[3] = {
+	killRank:3,
+	loyaltyPercent:0.170000
+}
+allianceFightLoyaltyGet[4] = {
+	killRank:4,
+	loyaltyPercent:0.140000
+}
+allianceFightLoyaltyGet[5] = {
+	killRank:5,
+	loyaltyPercent:0.140000
+}
+allianceFightLoyaltyGet[6] = {
+	killRank:6,
+	loyaltyPercent:0.110000
+}
+allianceFightLoyaltyGet[7] = {
+	killRank:7,
+	loyaltyPercent:0.110000
+}
+allianceFightLoyaltyGet[8] = {
+	killRank:8,
+	loyaltyPercent:0.080000
+}
+allianceFightLoyaltyGet[9] = {
+	killRank:9,
+	loyaltyPercent:0.080000
+}
+allianceFightLoyaltyGet[10] = {
+	killRank:10,
+	loyaltyPercent:0.050000
+}
