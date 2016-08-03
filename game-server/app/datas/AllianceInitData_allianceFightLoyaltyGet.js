e0f0ba98aafecdec180bb0e8abd1a7777311be66
Modun Zhang <modunzhang@gmail.com>
2016-08-03 10:34:28 +0800

    修改配置文件

--- game-server/app/datas/AllianceInitData_allianceFightLoyaltyGet.js
+++ game-server/app/datas/AllianceInitData_allianceFightLoyaltyGet.js
@@ -5,41 +5,41 @@ module.exports = allianceFightLoyaltyGet
 
 allianceFightLoyaltyGet[1] = {
 	killRank:1,
-	loyaltyPercent:0.200000
+	loyaltyPercent:0.100000
 }
 allianceFightLoyaltyGet[2] = {
 	killRank:2,
-	loyaltyPercent:0.170000
+	loyaltyPercent:0.080000
 }
 allianceFightLoyaltyGet[3] = {
 	killRank:3,
-	loyaltyPercent:0.170000
+	loyaltyPercent:0.070000
 }
 allianceFightLoyaltyGet[4] = {
 	killRank:4,
-	loyaltyPercent:0.140000
+	loyaltyPercent:0.050000
 }
 allianceFightLoyaltyGet[5] = {
 	killRank:5,
-	loyaltyPercent:0.140000
+	loyaltyPercent:0.045000
 }
 allianceFightLoyaltyGet[6] = {
 	killRank:6,
-	loyaltyPercent:0.110000
+	loyaltyPercent:0.040000
 }
 allianceFightLoyaltyGet[7] = {
 	killRank:7,
-	loyaltyPercent:0.110000
+	loyaltyPercent:0.035000
 }
 allianceFightLoyaltyGet[8] = {
 	killRank:8,
-	loyaltyPercent:0.080000
+	loyaltyPercent:0.030000
 }
 allianceFightLoyaltyGet[9] = {
 	killRank:9,
-	loyaltyPercent:0.080000
+	loyaltyPercent:0.025000
 }
 allianceFightLoyaltyGet[10] = {
 	killRank:10,
-	loyaltyPercent:0.050000
+	loyaltyPercent:0.020000
 }
