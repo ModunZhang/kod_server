c660074cb32a6b400c17e064242be86088a21203
Modun Zhang <modunzhang@gmail.com>
2016-08-04 16:11:29 +0800

    新增玩家进入服务器时的欢迎邮件

--- game-server/app/datas/Localizations_player.js
+++ game-server/app/datas/Localizations_player.js
@@ -35,25 +35,94 @@ player["ChatMuteNotice"] = {
 }
 player["welcomeMailTitle_wp"] = {
 	key:"welcomeMailTitle_wp",
-	cn:"wp中文欢迎标题",
-	en:"wp英文欢迎标题",
+	cn:"欢迎您加入《魔龙创世纪》！",
+	en:"Welcome to Dragonfall!",
 	tw:"wp繁体欢迎标题"
 }
 player["welcomeMailContent_wp"] = {
 	key:"welcomeMailContent_wp",
-	cn:"wp中文欢迎类容",
-	en:"wp英文欢迎类容",
+	cn:"亲爱的领主大人：\
+\
+    欢迎您加入即时策略游戏《魔龙创世纪》！\
+\
+    在这里，你可以：\
+\
+     收集物资，玩转城市建设，创造一个个性化的帝国\
+     丰富的兵种，强大的装备等待你来发掘\
+     训练并提升你的巨龙，带领史诗级的部队出征\
+     探索各式各样未知的领土\
+     与其他玩家组成联盟，共同征服敌人，建立最强王国\
+     便捷的语言翻译功能，与世界各地玩家共同制定游戏策略\
+\
+\
+     如果在游戏中遇到什么问题，您可以通过游戏内的“联系我们”和我们沟通。\
+ \
+     祝您游戏愉快！\
+\
+\
+魔龙创世纪 团队",
+	en:"Dear Lord:\
+\
+    Welcome to join Dragonfall!\
+\
+    Have you ever dreamed of commanding Dragons? Now, try it here in Dragonfall, a wonderful hard-core MMO strategy game! Play for free with Millions of players and make prudent choices for each step of your growth, or bear the risk of defeats! Join an Alliance to fight for the throne with the ultimate power of Dragons, win a place for your kingdom in this land of fantasia!\
+\
+    You, a heroic warlord of the Knights, died in the crusade against the Black Dragon. But for an unknown purpose the evil dragon resurrected you, granted you the mysterious ability of commanding other dragons. By coming back you find the Empire been ripped apart by betrayers, and the civilians suffer deeply from the war. Will you unify the broken Empire again? Will you be strong enough to take vengeance on the Black Dragon? Establish your kingdom, build your army, and prove yourself in the cruel war field!\
+\
+\
+    If you have any problems or suggestions, please feel free to contact us via the \"Contact Us\" in our game.\
+\
+    Thank you and have fun!\
+\
+\
+Dragonfall Team",
 	tw:"wp繁体欢迎类容"
 }
 player["welcomeMailTitle_ios"] = {
 	key:"welcomeMailTitle_ios",
-	cn:"ios中文欢迎标题",
-	en:"ios英文欢迎标题",
+	cn:"欢迎您加入《巨龙争霸》！",
+	en:"Welcome to Dragon War!",
 	tw:"ios繁体欢迎标题"
 }
 player["welcomeMailContent_ios"] = {
 	key:"welcomeMailContent_ios",
-	cn:"ios中文欢迎类容",
-	en:"ios英文欢迎类容",
+	cn:"亲爱的领主大人：\
+\
+    欢迎您加入即时策略游戏《巨龙争霸》！\
+\
+    在这里，你可以：\
+\
+     收集物资，玩转城市建设，创造一个个性化的帝国\
+     丰富的兵种，强大的装备等待你来发掘\
+     训练并提升你的巨龙，带领史诗级的部队出征\
+     探索各式各样未知的领土\
+     与其他玩家组成联盟，共同征服敌人，建立最强王国\
+     便捷的语言翻译功能，与世界各地玩家共同制定游戏策略\
+\
+\
+     如果在游戏中遇到什么问题，您可以通过游戏内的“联系我们”以及以下方式和我们沟通：\
+ \
+     官方玩家QQ群：575936673\
+     官方微信公众号：jituohudong\
+\
+      祝您游戏愉快！\
+\
+\
+巨龙争霸 团队",
+	en:"Dear Lord:\
+\
+    Welcome to join Dragon War!\
+\
+    Have you ever dreamed of commanding Dragons? Now, try it here in Dragon War, a wonderful hard-core MMO strategy game! Play for free with Millions of players and make prudent choices for each step of your growth, or bear the risk of defeats! Join an Alliance to fight for the throne with the ultimate power of Dragons, win a place for your kingdom in this land of fantasia!\
+\
+    You, a heroic warlord of the Knights, died in the crusade against the Black Dragon. But for an unknown purpose the evil dragon resurrected you, granted you the mysterious ability of commanding other dragons. By coming back you find the Empire been ripped apart by betrayers, and the civilians suffer deeply from the war. Will you unify the broken Empire again? Will you be strong enough to take vengeance on the Black Dragon? Establish your kingdom, build your army, and prove yourself in the cruel war field!\
+\
+\
+    If you have any problems or suggestions, please feel free to contact us via the \"Contact Us\" in our game.\
+\
+    Thank you and have fun!\
+\
+\
+Dragon War Team",
 	tw:"ios繁体欢迎类容"
 }
