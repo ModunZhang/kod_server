243e58720e0ffff02b8cc984af136abf5ef1c0bc
Modun Zhang <modunzhang@gmail.com>
2016-09-07 16:49:11 +0800

    修复Schema 定义错误

--- game-server/app/domains/player.js
+++ game-server/app/domains/player.js
@@ -752,7 +752,7 @@ var PlayerSchema = new Schema({
 		fromAllianceTag:{type:String, required:true},
 		toId:{type:String, required:true},
 		toName:{type:String, required:true},
-		toIcon:{type:String, required:true},
+		toIcon:{type:Number, required:true},
 		content:{type:String, required:true},
 		sendTime:{type:Number, required:true}
 	}],
