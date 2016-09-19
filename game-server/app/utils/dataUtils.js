44b6e608c720ac82b0b8bf46619ee3967e29d1b5
Modun Zhang <modunzhang@gmail.com>
2016-09-19 22:01:17 +0800

    修复当可用城民上限为0时招募士兵会造成服务器死循环的问题

--- game-server/app/utils/dataUtils.js
+++ game-server/app/utils/dataUtils.js
@@ -73,13 +73,16 @@ Utils.buyResources = function(playerDoc, need, has){
 			var currentBuy = 0
 			if(_.isEqual(key, "citizen")){
 				var freeCitizenLimit = self.getPlayerFreeCitizenLimit(playerDoc)
+				if(freeCitizenLimit <= 0){
+					freeCitizenLimit = 1;
+				}
 				while(required > 0){
 					var requiredPercent = required / freeCitizenLimit
 					for(i = config.length - 1; i >= 1; i--){
 						item = config[i]
 						if(item.min < requiredPercent){
 							gemUsed += item.gem
-							var citizenBuyed = Math.floor(item.resource * freeCitizenLimit)
+							var citizenBuyed = Math.ceil(item.resource * freeCitizenLimit)
 							required -= citizenBuyed
 							currentBuy += citizenBuyed
 							break
